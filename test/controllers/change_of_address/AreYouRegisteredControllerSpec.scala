272172a97c38773ab1ae88ec1919ede7d044bbe6
richarv <richarv@richarv-VirtualBox.(none)>
2014-01-23 11:49:23 +0000

    Controller and Integration spec tests added for AreYouRegistered page

---
+++ test/controllers/change_of_address/AreYouRegisteredControllerSpec.scala
@@ -0,0 +1,40 @@
+package controllers.change_of_address
+
+import org.specs2.mutable._
+import play.api.test.{FakeRequest, WithApplication}
+import play.api.test.Helpers._
+import controllers.change_of_address
+
+
+class AreYouRegisteredControllerSpec extends Specification with Tags {
+
+  "AreYouRegistered - Controller" should {
+
+
+    "present" in new WithApplication {
+      // Arrange
+      val request = FakeRequest().withSession()
+
+      // Act
+      val result = change_of_address.AreYouRegistered.present(request)
+
+      // Assert
+      status(result) mustEqual OK
+    }
+
+
+    "redirect to next page after the button is clicked" in new WithApplication {
+      // Arrange
+      val request = FakeRequest().withSession()
+
+      // Act
+      val result = change_of_address.AreYouRegistered.submit(request)
+
+      // Assert
+      status(result) mustEqual SEE_OTHER
+      redirectLocation(result) mustEqual (Some("/are-you-registered")) //TODO update with next page url
+    }
+
+  }
+
+}
--- test/controllers/change_of_address/AreYouRegisteredIntegrationSpec.scala
+++ test/controllers/change_of_address/AreYouRegisteredIntegrationSpec.scala
@@ -1,34 +1,31 @@
 
 package controllers.change_of_address
 
-
-
-
 import org.specs2.mutable.{Specification, Tags}
 import play.api.test.WithBrowser
 import controllers.BrowserMatchers
 
-class VerifyIdentityIntegrationSpec extends Specification with Tags {
+class AreYouRegisteredIntegrationSpec extends Specification with Tags {
 
-  "VerifyIdentity Integration" should {
+  "AreYouRegistered Integration" should {
     "be presented" in new WithBrowser with BrowserMatchers {
       // Arrange & Act
-      browser.goTo("/verify-identity")
+      browser.goTo("/are-you-registered")
 
       // Assert
-      titleMustContain("verify identity")
+      titleMustContain("are you registered")
 
     }
 
     "go to next page after the button is clicked" in new WithBrowser with BrowserMatchers {
       // Arrange
-      browser.goTo("/verify-identity")
+      browser.goTo("/are-you-registered")
 
       // Act
       browser.submit("button[type='submit']")
 
       // Assert
-      titleMustEqual("Change of keeper - are you registered") //TODO We need to change this to look at page3
+      titleMustEqual("Change of keeper - are you registered") //TODO We need to change this to look at page5
     }
 
   }
