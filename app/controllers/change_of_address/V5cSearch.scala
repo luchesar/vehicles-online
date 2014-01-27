bf436560c3787d55fd4cbe5a258b27aacefa9c37
Ian Stainer <ian.stainer@valtech.co.uk>
2014-01-27 12:38:35 +0000

    Extracted out the hard coded name in the message bundle in v5c-search page. It is now passed through as an argument

--- app/controllers/change_of_address/V5cSearch.scala
+++ app/controllers/change_of_address/V5cSearch.scala
@@ -11,12 +11,12 @@ import controllers.Mappings._
 object V5cSearch extends Controller {
 
   def present = Action { implicit request =>
-    Ok(html.change_of_address.v5c_search(v5cSearchForm))
+    Ok(html.change_of_address.v5c_search(v5cSearchForm, fetchData))
   }
 
   def submit = Action { implicit request =>
     v5cSearchForm.bindFromRequest.fold(
-      formWithErrors => BadRequest(html.change_of_address.v5c_search(formWithErrors)),
+      formWithErrors => BadRequest(html.change_of_address.v5c_search(formWithErrors, fetchData())),
         ToAddress => Redirect(routes.ConfirmVehicleDetails.present())
     )
   }
@@ -28,4 +28,7 @@ object V5cSearch extends Controller {
     )(V5cSearchModel.apply)(V5cSearchModel.unapply)
   )
 
+  def fetchData(): String = {
+    "Roger Booth"
+  }  
 }
