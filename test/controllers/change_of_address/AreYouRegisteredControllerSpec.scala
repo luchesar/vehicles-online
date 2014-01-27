122671b636dab69c3b7810929ff0bbf0d1ea35d8
Matthew Snaith <fault_line@hotmail.co.uk>
2014-01-27 15:12:11 +0000

    AuthenticationControllerSpec changed to scalatest

--- test/controllers/change_of_address/AreYouRegisteredControllerSpec.scala
+++ test/controllers/change_of_address/AreYouRegisteredControllerSpec.scala
@@ -1,16 +1,13 @@
 package controllers.change_of_address
 
-import org.specs2.mutable._
 import play.api.test.{FakeRequest, WithApplication}
 import play.api.test.Helpers._
 import controllers.change_of_address
 import org.scalatest.{Matchers, WordSpec}
 
-
 class AreYouRegisteredControllerSpec  extends WordSpec with Matchers {
 
   "AreYouRegistered - Controller" should {
-
 
     "present" in new WithApplication {
       // Arrange
@@ -23,7 +20,6 @@ class AreYouRegisteredControllerSpec  extends WordSpec with Matchers {
       status(result) should equal(OK)
     }
 
-
     "redirect to next page after the button is clicked" in new WithApplication {
       // Arrange
       val request = FakeRequest().withSession()
@@ -33,9 +29,7 @@ class AreYouRegisteredControllerSpec  extends WordSpec with Matchers {
 
       // Assert
       status(result) should equal(SEE_OTHER)
-      redirectLocation(result) should equal(Some("/sign-in-provider")) //TODO update with next page url
+      redirectLocation(result) should equal(Some("/sign-in-provider"))
     }
-
   }
-
 }
