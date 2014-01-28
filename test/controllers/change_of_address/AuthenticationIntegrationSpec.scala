f17e4e25567d00795b09f835e0c9aa9c3e0f9a83
Matthew Snaith <fault_line@hotmail.co.uk>
2014-01-28 08:19:22 +0000

    ToDo inserted to convert specs2 test to scalatest for AuthenticationIntegrationSpec, currently using specs2 as scalatest not working as expected

--- test/controllers/change_of_address/AuthenticationIntegrationSpec.scala
+++ test/controllers/change_of_address/AuthenticationIntegrationSpec.scala
@@ -1,9 +1,38 @@
 package controllers.change_of_address
 
-import org.scalatest.WordSpec
-import org.scalatest.Matchers
-import org.scalatest.selenium.{HtmlUnit}
+import org.specs2.mutable.{Tags, Specification}
+import play.api.test.WithBrowser
+import controllers.BrowserMatchers
 
+class AuthenticationIntegrationSpec extends Specification with Tags {
+
+  val PINValid = "123456"
+
+  "Authentication Integration" should {
+    "be presented" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      browser.goTo("/authentication")
+
+      // Assert
+      titleMustContain("authentication")
+
+    }
+
+    "go to next page after the button is clicked" in new WithBrowser with BrowserMatchers {
+      // Arrange
+      browser.goTo("/authentication")
+
+      // Act
+      browser.fill("#PIN") `with` PINValid
+      browser.submit("button[type='submit']")
+
+      // Assert
+      titleMustEqual("Change of keeper - retrieve a vehicle record")
+    }
+  }
+}
+
+//ToDo convert test from specs2 to scalatest
 /*class AuthenticationIntegrationSpec extends WordSpec with Matchers with HtmlUnit {
   val host = "http://localhost:9000/"
 
@@ -32,3 +61,5 @@ import org.scalatest.selenium.{HtmlUnit}
     }
   }
 }*/
+
+
