855a84fccf6d0b8977f742b295a47f5207435cd7
Ian Stainer <ian.stainer@valtech.co.uk>
2014-01-29 23:12:44 +0000

    Added comments

--- app/modules/package.scala
+++ app/modules/package.scala
@@ -15,11 +15,16 @@ package object modules {
   import scala.concurrent.ExecutionContext
   import ExecutionContext.Implicits.global
 
+  // Play.isTest will evaluate to true when you run "play test" from the command line
+  // If play is being run to execute the tests then use the TestModule to provide fake 
+  // implementations of traits otherwise use the DevModule to provide the real ones
   def module = if (Play.isTest) TestModule else DevModule
 
   lazy val injector = Guice.createInjector(module)
 
-
+  /**
+   * Provides real implementations for traits
+   */
   object DevModule extends ScalaModule {
     def configure() {
       Logger.debug("Guice is loading DevModule")
@@ -28,20 +33,32 @@ package object modules {
     }
   }
 
+  /**
+   * Provides fake or test implementations for traits
+   */
   object TestModule extends ScalaModule {
 
+    /**
+     * Fake implementation of the WebService trait with in memory data
+     */
     case class FakeV5cSearchResponseWebService() extends WebService {
       override def invoke(cmd: V5cSearchModel): Future[V5cSearchResponse] = Future {
         V5cSearchResponse(true, "All ok ", V5cSearchConfirmationModel("vrn", "make", "model", "firstRegistered", "acquired"))
       }
     }
 
+    /**
+     * Fake implementation of the LoginWebService trait with in memory data
+     */
     case class FakeLoginResponseWebService() extends LoginWebService {
       override def invoke(cmd: LoginPageModel): Future[LoginResponse] = Future {
         LoginResponse(true, "All ok ", LoginConfirmationModel("Roger", "Booth", "21/05/1977", Address(line1 = "115 Park Avenue", line4 = Some("United Kingdom"), postCode = "SA6 8HY")))
       }
     }
 
+    /**
+     * Bind the fake implementations the traits
+     */
     def configure() {
       Logger.debug("Guice is loading TestModule")
 
