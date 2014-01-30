39056dac8211b2ab92f6b1cb732ffa07645c71dc
richarv <vivrichards600@hotmail.com>
2014-01-30 16:16:26 +0000

    checked and amended controller tests after login page

--- test/controllers/change_of_address/LoginConfirmationControllerSpec.scala
+++ test/controllers/change_of_address/LoginConfirmationControllerSpec.scala
@@ -37,38 +37,15 @@ class LoginConfirmationControllerSpec extends WordSpec with Matchers with Mockit
     }
   }
 
-  "present v5c search page when user is logged in but has not entered vehicle details" in new WithApplication {
-    // Arrange
-
-    //TODO:add this to the helper as reusable code!
-    val address = mock[Address]
-    address.line1 returns "mock line1"
-    address.postCode returns "mock postcode"
-    val loginConfirmationModel = mock[LoginConfirmationModel]
-    loginConfirmationModel.firstName returns "mock firstName"
-    loginConfirmationModel.surname returns "mock surname"
-    loginConfirmationModel.address returns address
-    val key = Mappings.LoginConfirmationModel.key
-    play.api.cache.Cache.set(key, loginConfirmationModel)
-
-    val request = FakeRequest().withSession()
-
-    // Act
-    val result = change_of_address.ConfirmVehicleDetails.present(request)
-
-    // Assert
-    redirectLocation(result) should equal (Some("/v5c-search"))
-  }
-
   "present login page when user is not logged in" in new WithApplication {
     // Arrange
     val request = FakeRequest().withSession()
 
     // Act
-    val result = change_of_address.AreYouRegistered.present(request)
+    val result = change_of_address.LoginConfirmation.present(request)
 
     // Assert
-    status(result) should equal(OK)
+    redirectLocation(result) should equal(Some("/are-you-registered"))
   }
 
     "redirect to next page after the agree button is clicked" in new WithApplication {
