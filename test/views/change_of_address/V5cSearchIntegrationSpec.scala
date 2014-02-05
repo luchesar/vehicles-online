8e340a8cca97c89be8ac10ca0a4e4cc93c4e77c1
Scott Walker <scott.walker@valtech.co.uk>
2014-02-05 16:19:26 +0000

    Adding V5cReferenceNumber rejection integration tests, one is pending until we have disabled HTML5 validation

--- test/views/change_of_address/V5cSearchIntegrationSpec.scala
+++ test/views/change_of_address/V5cSearchIntegrationSpec.scala
@@ -1,4 +1,3 @@
-
 package views.change_of_address
 
 import org.specs2.mutable.{Specification, Tags}
@@ -48,5 +47,68 @@ class V5cSearchIntegrationSpec extends Specification with Tags {
       // Assert
       titleMustEqual("Change of keeper address")
     }
+
+    "reject submit when V5cReferenceNumber is blank" in new WithBrowser with BrowserMatchers {
+      //Arrange & Act
+      // Pass credentials through login page
+      TestHelper.loginPagePopulate(browser)
+
+      // Complete validation page by entering a pin
+      TestHelper.authenticationPopulate(browser)
+
+      // Complete V5c search page
+      TestHelper.v5cSearchPagePopulate(browser, "")
+
+      // Assert
+      findMustEqualSize("div[class=validation-summary] ol li", 3)
+    }
+
+    "reject submit when V5cReferenceNumber contains less than minimum characters" in new WithBrowser with BrowserMatchers {
+      //Arrange & Act
+      // Pass credentials through login page
+      TestHelper.loginPagePopulate(browser)
+
+      // Complete validation page by entering a pin
+      TestHelper.authenticationPopulate(browser)
+
+      // Complete V5c search page
+      TestHelper.v5cSearchPagePopulate(browser, "123")
+
+      // Assert
+      findMustEqualSize("div[class=validation-summary] ol li", 1)
+    }
+
+    "reject submit when V5cReferenceNumber contains more than maximum characters" in new WithBrowser with BrowserMatchers {
+      //Arrange & Act
+      // Pass credentials through login page
+      TestHelper.loginPagePopulate(browser)
+
+      // Complete validation page by entering a pin
+      TestHelper.authenticationPopulate(browser)
+
+      // Complete V5c search page
+      TestHelper.v5cSearchPagePopulate(browser, "12345678901234567890")
+
+      pending("to make this test work we need to disable HTML5 validation (in this case it is stopping the test from entering > 11 characters)")
+
+      // Assert
+      findMustEqualSize("div[class=validation-summary] ol li", 1)
+    } // TODO Make this test work, see pending message
+
+    "reject submit when V5cReferenceNumber contains letters" in new WithBrowser with BrowserMatchers {
+      //Arrange & Act
+      // Pass credentials through login page
+      TestHelper.loginPagePopulate(browser)
+
+      // Complete validation page by entering a pin
+      TestHelper.authenticationPopulate(browser)
+
+      // Complete V5c search page
+      TestHelper.v5cSearchPagePopulate(browser, "qwertyuiopa")
+
+      // Assert
+      findMustEqualSize("div[class=validation-summary] ol li", 1)
+    }
+
   }
 }
