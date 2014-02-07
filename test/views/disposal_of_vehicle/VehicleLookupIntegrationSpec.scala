c71da827fef0df71ad23180a8bd85013da49195a
Matthew Snaith <fault_line@hotmail.co.uk>
2014-02-07 11:53:51 +0000

    Further tests added to VehicleLookupIntegrationSpec

--- test/views/disposal_of_vehicle/VehicleLookupIntegrationSpec.scala
+++ test/views/disposal_of_vehicle/VehicleLookupIntegrationSpec.scala
@@ -34,5 +34,85 @@ class VehicleLookupIntegrationSpec extends Specification with Tags {
       checkNumberOfValidationErrors(3)
     }
 
+    "display two validation error messages when no v5cRegistrationNumber is entered" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cVehicleRegistrationNumber = "")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(2)
+    }
+
+    "display one validation error message when a v5cRegistrationNumber is entered containing one character" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cVehicleRegistrationNumber = "a")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(1)
+    }
+
+    "display one validation error message when a v5cRegistrationNumber is entered containing special characters" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cVehicleRegistrationNumber = "$^")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(1)
+    }
+
+    "display one validation error message when no v5cKeeperName is entered" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cKeeperName = "")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(2)
+    }
+
+    "display one validation error message when a v5cKeeperName is entered which is greater than max length" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cKeeperName = "qwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopq")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(1)
+    }
+
+    "display three validation error messages when no postcode is entered" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cPostcode = "")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(3)
+    }
+
+    "display two validation error messages when a postcode less than min length is entered" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cPostcode = "SA99")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(2)
+    }
+
+    "display one validation error message when a postcode containing a special character is entered" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cPostcode = "SA991B%")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(1)
+    }
+
+    "display one validation error message when a postcode containing an incorrect format" in new WithBrowser with BrowserMatchers {
+      // Arrange & Act
+      vehicleLookupIntegrationHelper(browser, v5cPostcode = "SA9999")
+
+      //Assert
+      titleMustContain("Dispose a vehicle into the motor trade")
+      checkNumberOfValidationErrors(1)
+    }
   }
 }
