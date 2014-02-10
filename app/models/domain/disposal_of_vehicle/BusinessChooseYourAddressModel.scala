79b24fa78156abcd12795a2437a945cf6e7de57d
Scott Walker <scott.walker@valtech.co.uk>
2014-02-10 15:11:49 +0000

    BusinessChooseYourAddress integration tests for address select

--- app/models/domain/disposal_of_vehicle/BusinessChooseYourAddressModel.scala
+++ app/models/domain/disposal_of_vehicle/BusinessChooseYourAddressModel.scala
@@ -1,3 +1,3 @@
 package models.domain.disposal_of_vehicle
 
-case class BusinessChooseYourAddressModel(businessName: String)
+case class BusinessChooseYourAddressModel(businessName: String, addressSelected: String)
