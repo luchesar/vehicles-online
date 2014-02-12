591b09f91d9a51c30cbe38aa61c5755785e4bfdf
Matthew Snaith <fault_line@hotmail.co.uk>
2014-02-12 11:08:44 +0000

    BusinessName chache checked for VehicleLookup, appropriate tests amended and passing

--- test/controllers/disposal_of_vehicle/VehicleLookupControllerSpec.scala
+++ test/controllers/disposal_of_vehicle/VehicleLookupControllerSpec.scala
@@ -5,6 +5,7 @@ import play.api.test.Helpers._
 import controllers.disposal_of_vehicle
 import org.scalatest.{Matchers, WordSpec}
 import mappings.disposal_of_vehicle.VehicleLookup._
+import helpers.disposal_of_vehicle.SetUpTradeDetailsPopulate
 
 class VehicleLookupControllerSpec extends WordSpec with Matchers {
 
@@ -12,6 +13,7 @@ class VehicleLookupControllerSpec extends WordSpec with Matchers {
 
     "present" in new WithApplication {
       // Arrange
+      SetUpTradeDetailsPopulate.setupCache
       val request = FakeRequest().withSession()
 
       // Act
@@ -26,8 +28,9 @@ class VehicleLookupControllerSpec extends WordSpec with Matchers {
       val v5cReferenceNumberValid = "12345678910"
       val v5cRegistrationNumberValid = "ABC123"
       val v5cKeeperNameValid = "John"
-      val v5cPostcodeValid = "SA99 1BD"
+      val v5cPostcodeValid = "SA99 1BD" //TODO make use for helper values
 
+      SetUpTradeDetailsPopulate.setupCache
       val request = FakeRequest().withSession()
         .withFormUrlEncodedBody(v5cReferenceNumberId -> v5cReferenceNumberValid, v5cRegistrationNumberId -> v5cRegistrationNumberValid, v5cKeeperNameId -> v5cKeeperNameValid, v5cPostcodeId -> v5cPostcodeValid)
 
@@ -36,7 +39,18 @@ class VehicleLookupControllerSpec extends WordSpec with Matchers {
 
       // Assert
       status(result) should equal(SEE_OTHER)
-      redirectLocation(result) should equal (Some("/disposal-of-vehicle/dispose")) //TODO: This needs to not look at itself but at the next page
+      redirectLocation(result) should equal (Some("/disposal-of-vehicle/dispose"))
      }
+
+    "redirect to setupTradeDetails page when user is not logged in" in new WithApplication {
+      // Arrange
+      val request = FakeRequest().withSession()
+
+      // Act
+      val result = disposal_of_vehicle.VehicleLookup.present(request)
+
+      // Assert
+      redirectLocation(result) should equal(Some("/disposal-of-vehicle/setup-trade-details"))
+    }
   }
 }
