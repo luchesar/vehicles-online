77574057b7ed7a0bd9917049121055755c1c1c1e
Ian Stainer <ian.stainer@valtech.co.uk>
2014-02-12 11:09:35 +0000

    We now redirect to set up trade details before if user goes directly to dispose confirmation

--- test/views/disposal_of_vehicle/DisposeConfirmationIntegrationSpec.scala
+++ test/views/disposal_of_vehicle/DisposeConfirmationIntegrationSpec.scala
@@ -3,7 +3,7 @@ package views.disposal_of_vehicle
 import org.specs2.mutable.{Tags, Specification}
 import play.api.test.WithBrowser
 import controllers.BrowserMatchers
-import app.DisposalOfVehicle.Dispose._
+import helpers.disposal_of_vehicle.SetUpTradeDetailsPopulate
 
 class DisposeConfirmationIntegrationSpec extends Specification with Tags {
 
@@ -12,10 +12,19 @@ class DisposeConfirmationIntegrationSpec extends Specification with Tags {
   "Dispose confirmation integration" should {
     "be presented" in new WithBrowser with BrowserMatchers {
       // Arrange & Act
+      SetUpTradeDetailsPopulate.happyPath(browser)
       browser.goTo(disposeConfirmationUrl)
 
       // Check the page title is correct
       titleMustEqual("Dispose a vehicle into the motor trade: summary")
     }
+  }
+
+  "Redirect when no traderBusinessName is cached" in new WithBrowser with BrowserMatchers {
+    // Arrange & Act
+    browser.goTo(disposeConfirmationUrl)
+
+    // Assert
+    titleMustEqual("Dispose a vehicle into the motor trade: set-up")
   }
 }
