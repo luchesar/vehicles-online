d181be4de34fdb7a09aac5d477c8967ac85a6d8c
Ian Stainer <ian.stainer@valtech.co.uk>
2014-02-16 20:23:25 +0000

    Added comments about guice IoC

--- app/modules/DevModule.scala
+++ app/modules/DevModule.scala
@@ -6,7 +6,14 @@ import services._
 import modules.TestModule.FakeAddressLookupService
 
 /**
- * Provides real implementations for traits
+ * Provides real implementations of traits
+ * Note the use of sse-guice, which is a library that makes the Guice internal DSL more scala friendly
+ * eg we can write this:
+ * bind[Service].to[ServiceImpl].in[Singleton]
+ * instead of this:
+ * bind(classOf[Service]).to(classOf[ServiceImpl]).in(classOf[Singleton])
+ *
+ * Look in build.scala for where we import the sse-guice library
  */
 object DevModule extends ScalaModule {
   def configure() {
