625e479a43b42fc33df1d356419d93197b28788a
Scott Walker <scott.walker@valtech.co.uk>
2014-02-21 11:51:32 +0000

    Added UPRN to AddressAndPostcode model, updated mapping, this is NOT visible in the view

--- app/mappings/disposal_of_vehicle/AddressAndPostcode.scala
+++ app/mappings/disposal_of_vehicle/AddressAndPostcode.scala
@@ -6,12 +6,15 @@ import mappings.disposal_of_vehicle.AddressLines._
 import constraints.AddressLines._
 import mappings.common.PostCode._
 import models.domain.disposal_of_vehicle.AddressAndPostcodeModel
+import mappings.common.Uprn
+import mappings.common.Uprn.uprn
 
 object AddressAndPostcode {
   val id = "addressAndPostcode"
   val postcodeMaxLength = "9"
 
   val addressAndPostcode: Mapping[AddressAndPostcodeModel] = mapping(
+    Uprn.id -> uprn,
     AddressLines.id -> addressLines.verifying(validAddressLines),
     postcodeId -> postcode()
   )(AddressAndPostcodeModel.apply)(AddressAndPostcodeModel.unapply)
