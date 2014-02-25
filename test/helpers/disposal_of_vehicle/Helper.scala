6d90bd2886b633d5eaf0f7364e93a9cafa171be1
Matthew Snaith <fault_line@hotmail.co.uk>
2014-02-25 09:02:11 +0000

    started writting drop down list test for BusinessChooseYourAddressIntegrationSpec

--- test/helpers/disposal_of_vehicle/Helper.scala
+++ test/helpers/disposal_of_vehicle/Helper.scala
@@ -4,11 +4,11 @@ object Helper {
   val v5cDocumentReferenceNumberValid = "12345678910"
   val v5cVehicleRegistrationNumberValid = "AB12AWR"
   val v5cKeeperNameValid = "John Smith"
-  val v5cPostcodeValid = "SA991DD"
+  val v5cPostcodeValid = "CM81QJ"
   val v5ckey = v5cDocumentReferenceNumberValid + "." + v5cVehicleRegistrationNumberValid
 
   val traderBusinessNameValid = "example trader name"
-  val traderPostcodeValid = "SA99 1DD"
+  val traderPostcodeValid = "CM81QJ"
   val traderaddressValid = "1"
 
   val consentValid = "true"
