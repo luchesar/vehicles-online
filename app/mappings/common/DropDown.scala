154b09627979ebccceb5d3474fc5fa7c26ee6406
Scott Walker <scott.walker@valtech.co.uk>
2014-02-25 08:52:13 +0000

    DropDown maxLength 9999 comes from a constant in mappings. Deleted duplicate template

--- app/mappings/common/DropDown.scala
+++ app/mappings/common/DropDown.scala
@@ -4,11 +4,13 @@ import play.api.data.Mapping
 import play.api.data.Forms._
 
 object DropDown {
+  val maxLength = 9999 // TODO find out from BAs the maxLength for address in GDS DB
+
   def dropDown: Mapping[String] = {
-    nonEmptyText(maxLength = 9999) // TODO find the maxLength for address in GDS DB
+    nonEmptyText(maxLength = 9999)
   }
 
   def dropDown(dropDownOptions: Map[String, String]): Mapping[String] = {
-    nonEmptyText(maxLength = 9999) verifying constraints.DropDown.rules(dropDownOptions) // TODO find the maxLength for address in GDS DB
+    nonEmptyText(maxLength = 9999) verifying constraints.DropDown.rules(dropDownOptions)
   }
 }
