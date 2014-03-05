be1466e618a8328f61c37b1ef3f0dd8f2fbf3bb8
Sion Dafydd <sion.dafydd@valtech.co.uk>
2014-03-05 16:47:03 +0000

    fix issue with multiple browsers not working

--- test/pages/disposal_of_vehicle/BeforeYouStartPage.scala
+++ test/pages/disposal_of_vehicle/BeforeYouStartPage.scala
@@ -7,7 +7,7 @@ import helpers.webbrowser._
 object BeforeYouStartPage extends Page with WebBrowser {
 
   override val url: String = WebDriverFactory.baseUrl
-  override val title: String = "Dispose a vehicle into the motor trade: start"
+  override val title: String = "Dispose a vehicle into the motor trade: set-up"
 
   def startNow(implicit driver: WebDriver): Element = find(id("next")).get
 }
