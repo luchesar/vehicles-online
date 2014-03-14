985ff389593c12503387a8c5cf1f3cce41f04d86
Scott Walker <scott.walker@valtech.co.uk>
2014-03-14 12:46:15 +0000

    Ordnance Survey address lookup - Improved sorting, copes with house numbers that contain letters, sorting extracted to method, put TODOs for unit tests

--- app/services/address_lookup/ordnance_survey/AddressLookupServiceImpl.scala
+++ app/services/address_lookup/ordnance_survey/AddressLookupServiceImpl.scala
@@ -5,7 +5,7 @@ import models.domain.disposal_of_vehicle.AddressViewModel
 import utils.helpers.Config
 import play.api.Logger
 import com.ning.http.client.Realm.AuthScheme
-import services.address_lookup.ordnance_survey.domain.{OSAddressbaseResult, OSAddressbaseSearchResponse}
+import services.address_lookup.ordnance_survey.domain.{OSAddressbaseDPA, OSAddressbaseResult, OSAddressbaseSearchResponse}
 import scala.concurrent.{Future, ExecutionContext}
 import ExecutionContext.Implicits.global
 import javax.inject.Inject
@@ -39,26 +39,32 @@ class AddressLookupServiceImpl @Inject()(ws: services.WebService) extends Addres
     response.flatMap(_.results)
   }
 
+  // TODO write unit test that lists are sorted by building number
+  // TODO write unit test that it does not throw when list contains a building number that contains letters.
+  // TODO write unit test that lists are sorted by building number then building name
+  def sort(addresses: Seq[OSAddressbaseDPA]) = {
+    addresses.sortBy(addressDpa => {
+      val buildingNumber = addressDpa.buildingNumber.getOrElse("0")
+      val buildingNumberSanitised = if (buildingNumber.matches("[+-]?\\d+")) buildingNumber.toInt else 0 // Sanitise building number as it could contain letters which would cause toInt to throw e.g. 107a.
+      (buildingNumberSanitised, addressDpa.buildingName) // TODO check with BAs how they would want to sort the list
+    })
+  }
+
   override def fetchAddressesForPostcode(postcode: String): Future[Seq[(String, String)]] = {
-    def toUprnsAndAddresses(resp: Response): Seq[(String, String)] = {
+    def toUprnsAndAddresses(resp: Response): Seq[(String, String)] =
       extractFromJson(resp) match {
-        case Some(r) =>
-          r.flatMap { address =>
-            address.DPA match {
-              case Some(dpa) => Some((dpa.UPRN, dpa.address))
-              case _ => None // TODO check if an LPI entry is present
-            }
-          }
+        case Some(results) =>
+          val addresses = results.flatMap { _.DPA }
+          sort(addresses) map { address => (address.UPRN, address.address) } // Sort before translating to view models.
         case None =>
           // Handle no results
           Logger.debug(s"No results returned for postcode: $postcode")
           Seq.empty
       }
-    }
 
     callPostcodeWebService(postcode).map { resp =>
       Logger.debug(s"Http response code from Ordnance Survey postcode lookup service was: ${resp.status}")
-      if (resp.status == play.api.http.Status.OK) toUprnsAndAddresses(resp).sortBy(x => x._1) // Sort by UPRN. TODO check with BAs how they would want to sort the list
+      if (resp.status == play.api.http.Status.OK) toUprnsAndAddresses(resp)
       else Seq.empty // The service returned http code other than 200
     }.recover {
       case e: Throwable =>
@@ -69,23 +75,17 @@ class AddressLookupServiceImpl @Inject()(ws: services.WebService) extends Addres
 
   override def fetchAddressForUprn(uprn: String): Future[Option[AddressViewModel]] = {
     // Extract result from response and return as a view model.
-    def toAddressViewModel(resp: Response) = {
-
+    def toAddressViewModel(resp: Response) =
       extractFromJson(resp) match {
         case Some(results) =>
-          val addressViewModels = results.flatMap { address =>
-            address.DPA match {
-              case Some(dpa) => Some(AddressViewModel(uprn = Some(dpa.UPRN.toLong), address = dpa.address.split(",")))
-              case _ => None // TODO check if an LPI entry is present
-            }
-          }
+          val addresses = results.flatMap { _.DPA }
+          val addressViewModels = sort(addresses) map { address => AddressViewModel(uprn = Some(address.UPRN.toLong), address = address.address.split(", ")) } // Sort before translating to view models.
           require(addressViewModels.length >= 1, s"Should be at least one address for the UPRN: $uprn")
           Some(addressViewModels.head)
         case None =>
           Logger.error(s"No results returned by web service for submitted UPRN: $uprn")
           None
       }
-    }
 
     callUprnWebService(uprn).map { resp =>
       Logger.debug(s"Http response code from Ordnance Survey uprn lookup service was: ${resp.status}")
