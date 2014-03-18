ac4963cc730b0f2c09abd3d1ca0f827b28306024
James Chapman <james@developmentdoneright.co.uk>
2014-03-18 10:48:08 +0000

    Fix test following change of title for Before You Start

--- test/helpers/disposal_of_vehicle/BeforeYouStartPage.scala
+++ test/helpers/disposal_of_vehicle/BeforeYouStartPage.scala
@@ -2,5 +2,5 @@ package helpers.disposal_of_vehicle
 
 object BeforeYouStartPage {
   val url = "/disposal-of-vehicle/before-you-start"
-  val title = "Dispose a vehicle into the motor trade"
+  val title = "Sell a vehicle into the motor trade"
 }
