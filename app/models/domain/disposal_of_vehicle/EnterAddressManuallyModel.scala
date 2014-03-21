aaa516a11a596f0fa150df2afe03c3f0df5469f6
Matthew Snaith <fault_line@hotmail.co.uk>
2014-03-21 09:05:24 +0000

    implemented strip character method for use with optional lines of manual address entry
    Former-commit-id: fd30756b504169c2e1ba54ba434d0ba391a31942

--- app/models/domain/disposal_of_vehicle/EnterAddressManuallyModel.scala
+++ app/models/domain/disposal_of_vehicle/EnterAddressManuallyModel.scala
@@ -6,28 +6,19 @@ import mappings.disposal_of_vehicle.EnterAddressManually._
 case class EnterAddressManuallyModel(addressAndPostcodeModel: AddressAndPostcodeModel) {
   def stripPunctuation = {
 
+    def stripLine(inputline: Option[String]) = {
+      inputline match {
+        case Some(line) => {
+          Some(line.replace(invalidCharacter1, StringEmpty).replace(invalidCharacter2, StringEmpty))
+        }
+        case _ => None
+      }
+    }
+
     val line1Stripped = addressAndPostcodeModel.addressLinesModel.line1.replace(invalidCharacter1, StringEmpty)replace(invalidCharacter2, StringEmpty)
-
-    val line2Stripped = addressAndPostcodeModel.addressLinesModel.line2 match {
-      case Some(line) => {
-        Some(line.replace(invalidCharacter1, StringEmpty).replace(invalidCharacter2, StringEmpty))
-      }
-      case _ => None
-    }
-
-    val line3Stripped = addressAndPostcodeModel.addressLinesModel.line3 match {
-      case Some(line) => {
-        Some(line.replace(invalidCharacter1, StringEmpty).replace(invalidCharacter2, StringEmpty))
-      }
-      case _ => None
-    }
-
-    val line4Stripped = addressAndPostcodeModel.addressLinesModel.line4 match {
-      case Some(line) => {
-        Some(line.replace(invalidCharacter1, StringEmpty).replace(invalidCharacter2, StringEmpty))
-      }
-      case _ => None
-    }
+    val line2Stripped = stripLine(addressAndPostcodeModel.addressLinesModel.line2)
+    val line3Stripped = stripLine(addressAndPostcodeModel.addressLinesModel.line3)
+    val line4Stripped = stripLine(addressAndPostcodeModel.addressLinesModel.line4)
 
     copy(addressAndPostcodeModel =
       addressAndPostcodeModel.copy(addressLinesModel = AddressLinesModel(line1Stripped, line2Stripped, line3Stripped, line4Stripped)))
