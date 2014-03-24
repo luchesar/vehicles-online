01773f209e0256d0c8065c2a5c29535646e05ca3
Matthew Snaith <fault_line@hotmail.co.uk>
2014-03-24 15:18:17 +0000

    refactored methods into EnterAddressManuallyModel as these will not be used elsewhere
    Former-commit-id: 06124b86c2e6f6d9b9b988db9fec9c5484a30ad2

--- app/models/domain/disposal_of_vehicle/EnterAddressManuallyModel.scala
+++ app/models/domain/disposal_of_vehicle/EnterAddressManuallyModel.scala
@@ -1,10 +1,33 @@
 package models.domain.disposal_of_vehicle
 
 import models.domain.common.{AddressLinesModel, AddressAndPostcodeModel}
-import mappings.common.StripFromString._
-import mappings.disposal_of_vehicle.EnterAddressManually
 
 case class EnterAddressManuallyModel(addressAndPostcodeModel: AddressAndPostcodeModel) {
+
+  def stripEndOfLineRequiredField(inputline: String): String = {
+    inputline.takeRight(1) match {
+      case (",") => inputline.dropRight(1)
+      case (".") => inputline.dropRight(1)
+      case (",.") => inputline.dropRight(2)
+      case (".,") => inputline.dropRight(2)
+      case _ => inputline
+    }
+  }
+
+  def stripEndOfLineOptionalField(inputline: Option[String]): Option[String] = {
+    inputline match {
+      case Some(inputline) => {
+        inputline.takeRight(1) match {
+          case (",") => Some(inputline.dropRight(1))
+          case (".") => Some(inputline.dropRight(1))
+          case (",.") => Some(inputline.dropRight(2))
+          case (".,") => Some(inputline.dropRight(2))
+          case _ => Some(inputline)
+        }
+      }
+      case _ => None
+    }
+  }
 
   def stripCharsNotAccepted = {
 
@@ -12,12 +35,6 @@ case class EnterAddressManuallyModel(addressAndPostcodeModel: AddressAndPostcode
     val line2 = stripEndOfLineOptionalField(addressAndPostcodeModel.addressLinesModel.line2)
     val line3 = stripEndOfLineOptionalField(addressAndPostcodeModel.addressLinesModel.line3)
     val line4 = stripEndOfLineOptionalField(addressAndPostcodeModel.addressLinesModel.line4)
-
-    // code below is no longer being used -- keep for now if business rules require us to strip any characters from inside the line of each address
-    //val line1Stripped = stripLineRequiredField(line1, EnterAddressManually.charsNotAcceptedInLine)
-    //val line2Stripped = stripLineOptionalField(addressAndPostcodeModel.addressLinesModel.line2, EnterAddressManually.charsNotAcceptedInLine)
-    //val line3Stripped = stripLineOptionalField(addressAndPostcodeModel.addressLinesModel.line3, EnterAddressManually.charsNotAcceptedInLine)
-    //val line4Stripped = stripLineOptionalField(addressAndPostcodeModel.addressLinesModel.line4, EnterAddressManually.charsNotAcceptedInLine)
 
     copy(addressAndPostcodeModel = addressAndPostcodeModel.copy(addressLinesModel = AddressLinesModel(line1, line2, line3, line4)))
  }
