e14d1455bc22213ea191ccbb00b6f1cf6ff8f215
Scott Walker <scott.walker@valtech.co.uk>
2014-03-31 11:04:46 +0100

    Moved json implicits from VehicleLookupServiceImpl to the models
    Former-commit-id: b31610f0d22938022c41e4f2efb52da372bd7bfa

--- app/models/domain/disposal_of_vehicle/VehicleDetailsModel.scala
+++ app/models/domain/disposal_of_vehicle/VehicleDetailsModel.scala
@@ -1,3 +1,9 @@
 package models.domain.disposal_of_vehicle
 
+import play.api.libs.json.Json
+
 case class VehicleDetailsModel(registrationNumber: String, vehicleMake: String, vehicleModel: String, keeperName: String, keeperAddress: AddressViewModel)
+
+object VehicleDetailsModel {
+  implicit val vehicleDetailsModel = Json.format[VehicleDetailsModel]
+}
--- app/models/domain/disposal_of_vehicle/VehicleDetailsResponse.scala
+++ app/models/domain/disposal_of_vehicle/VehicleDetailsResponse.scala
@@ -1,3 +1,9 @@
 package models.domain.disposal_of_vehicle
 
+import play.api.libs.json.Json
+
 case class VehicleDetailsResponse (success: Boolean, message: String, vehicleDetailsModel: VehicleDetailsModel)
+
+object VehicleDetailsResponse {
+  implicit val vehicleDetailsResponse = Json.format[VehicleDetailsResponse]
+}
