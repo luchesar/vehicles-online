071bf58eb1d32c50394c7730e5f3710593956f65
Scott Walker <scott.walker@valtech.co.uk>
2014-03-31 10:54:19 +0100

    Renamed 'regex' to 'whitelist'. Replaced a match with an if because scala is inefficient doing match on bool
    Former-commit-id: c450518a387c512cfffc3469da177d63005addba

--- app/constraints/NumberOnly.scala
+++ app/constraints/NumberOnly.scala
@@ -4,8 +4,8 @@ import play.api.data.validation.{ValidationError, Invalid, Valid, Constraint}
 
 object NumberOnly {
   def rules: Constraint[String] = Constraint("constraint.restrictedvalidNumberOnly") { input =>
-    val inputRegex = """^\d[0-9]*$""".r
-    inputRegex.pattern.matcher(input).matches match {
+    val whitelist = """^\d[0-9]*$""".r
+    whitelist.pattern.matcher(input).matches match {
       case true => Valid
       case false => Invalid(ValidationError("error.restricted.validNumberOnly"))
     }
--- app/constraints/common/AddressLines.scala
+++ app/constraints/common/AddressLines.scala
@@ -8,9 +8,9 @@ object AddressLines {
   def validAddressLines: Constraint[AddressLinesModel] = Constraint[AddressLinesModel]("constraint.required") {
     case input@AddressLinesModel(line1, _, _, _) => {
       // Reject when: Too many characters, HTML or XML markup. Regex states string must contain at least one number or letter, can also include punctuation
-      val inputRegex = """^(?=.*[a-zA-Z0-9])[A-Za-z0-9\s\-\,\.]*$""".r
+      val whitelist = """^(?=.*[a-zA-Z0-9])[A-Za-z0-9\s\-\,\.]*$""".r
       if (input.totalCharacters >= maxLengthOfLinesConcatenated) Invalid(ValidationError("error.address.maxLengthOfLinesConcatenated"))
-      else if (!inputRegex.pattern.matcher(input.toViewFormat.mkString).matches) Invalid(ValidationError("error.address.characterinvalid"))
+      else if (!whitelist.pattern.matcher(input.toViewFormat.mkString).matches) Invalid(ValidationError("error.address.characterinvalid"))
       else Valid
     }
     case _ => Invalid(ValidationError("error.address.line1Required"))
