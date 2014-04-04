e075af550d5c20c65ee6042712951ef006d84d62
Azul <mail@azulinho.com>
2014-04-04 11:39:23 +0100

    refactored vehicles-online RPM, added vagrantfile and spec tests
    Former-commit-id: d14d8d6436f91594697e0db39ccf079031e3b88f

---
+++ microservice_spec.rb
@@ -0,0 +1,34 @@
+require_relative './spec_helper.rb'
+
+describe file('/opt/vehicles-online-1.0-SNAPSHOT/conf/vehiclesOnline.conf') do
+  its(:content) { should include 'ordnancesurvey'  }
+ end
+
+upstart_services = [ 'vehicles-online' ]
+
+upstart_services.each do |item|
+  describe command("initctl status #{item}") do
+    its(:stdout) {should include "start/running"}
+  end
+end
+
+
+sysV_services = [ 'monit' ]
+
+sysV_services.each do |item|
+  describe service(item) do
+    it { should be_running }
+  end
+end
+
+
+ports = [ 9000 ]
+
+ports.each do |item|
+  describe command('netstat -anl | grep LISTEN | grep -v ING') do
+    its(:stdout) {should include "#{item}" }
+  end
+end
+
+
+
