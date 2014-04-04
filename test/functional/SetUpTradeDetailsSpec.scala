fdc6be6aa5bb61610e6148ef86287ea61c9de53a
Scott Walker <scott.walker@valtech.co.uk>
2014-04-04 11:53:13 +0100

    US158 Added functional spec for happy path
    Former-commit-id: c48ad18ccdfd866fcbd15f3b2108455714f9972c

--- test/functional/SetUpTradeDetailsSpec.scala
+++ test/functional/SetUpTradeDetailsSpec.scala
@@ -3,33 +3,52 @@ package functional
 import org.scalatest.{BeforeAndAfterAll, Matchers, GivenWhenThen, FeatureSpec}
 import pages.disposal_of_vehicle._
 import helpers.webbrowser._
+import helpers.disposal_of_vehicle.Helper._
 
 class SetUpTradeDetailsSpec extends FeatureSpec with GivenWhenThen with Matchers with BeforeAndAfterAll with TestHarness {
 
-  feature("Dispose of a vehicle to trade") {
-      info("As a Motor Trader")
-      info("I want to receive an appropriate error message when I enter a disallowed Business Name")
-      info("so that I can move to the next step in the transaction")
-      info("")
+  feature("US158: Disposal to Trade: Business Name - Error Messages") {
+    info("As a Motor Trader")
+    info("I want to receive an appropriate error message when I enter a disallowed Business Name")
+    info("so that I can move to the next step in the transaction")
+    info("")
 
-      scenario("US158: Disposal to Trade: Business Name - Error Messages") {
+    scenario("No name entered") {
+      new WebBrowser {
+        Given("the motor trader has entered a Business Name")
+        go to BeforeYouStartPage
+        click on BeforeYouStartPage.startNow
+        // Note: leave the dealerName field blank to cause errors.
+        SetupTradeDetailsPage.dealerPostcode enter postcodeValid
 
-        new WebBrowser {
-          Given("the motor trader has entered a Business Name")
-          go to BeforeYouStartPage
-          click on BeforeYouStartPage.startNow
-          // Note: leave the dealerName field blank to cause errors.
-          SetupTradeDetailsPage.dealerPostcode enter "CM8 1QJ"
+        When("the Business Name does not comply with formatting rules")
+        click on SetupTradeDetailsPage.lookup
 
-          When("the Business Name does not comply with formatting rules")
-          click on SetupTradeDetailsPage.lookup
+        Then("a single appropriate message is displayed")
+        page.text should include("Must be between two and 30 characters and not contain invalid characters")
 
-          Then("a single appropriate message is displayed")
-          page.text should include("Must be between two and 30 characters and not contain invalid characters")
-
-          And("the motor trader remains on the current step in the")
-          page.title should equal("Provide your trader details")
-        }
+        And("the motor trader remains on the current step in the")
+        page.title should equal("Provide your trader details")
       }
     }
+
+    scenario("Valid name entered") {
+      new WebBrowser {
+        Given("the motor trader has entered a Business Name")
+        go to BeforeYouStartPage
+        click on BeforeYouStartPage.startNow
+        SetupTradeDetailsPage.dealerName enter traderBusinessNameValid
+        SetupTradeDetailsPage.dealerPostcode enter postcodeValid
+
+        When("the Business Name does comply with formatting rules")
+        click on SetupTradeDetailsPage.lookup
+
+        Then("the Business Name is retained")
+        page.text should include(traderBusinessNameValid)
+
+        And("the motor trader progresses to the next step in the transaction")
+        page.title should equal("Select your trade address")
+      }
+    }
+  }
 }
