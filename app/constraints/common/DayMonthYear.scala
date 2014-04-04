1da0fd3ee4fb2cb3860e13bc28f2f995ca00d78f
Scott Walker <scott.walker@valtech.co.uk>
2014-04-04 17:23:37 +0100

    US125 date of disposal cannot be more than 2 years in the past. Need to test 2 years and one day instead of 3 years to confirm corner case is covered. Need a fake DateService for the TestModule
    Former-commit-id: e5ed662b56ed0f181978c26cd95b1dc95bfce936

--- app/constraints/common/DayMonthYear.scala
+++ app/constraints/common/DayMonthYear.scala
@@ -4,6 +4,7 @@ import play.api.data.validation._
 import scala.util.{Failure, Success, Try}
 import org.joda.time.DateTime
 import play.api.data.validation.ValidationError
+import services.DateService
 
 object DayMonthYear {
   def required: Constraint[Int] = Constraint[Int]("constraint.required") {
@@ -21,5 +22,13 @@ object DayMonthYear {
     case Success(dt: DateTime) if dt.getYear > 9999 || dt.getYear < 999 => Invalid(ValidationError("error.invalid"))
     case Success(dt: DateTime) => Valid
     case Failure(_) => Invalid(ValidationError("error.invalid"))
+  }
+
+  def withinTwoYears(dateService: DateService): Constraint[models.DayMonthYear] = {
+    import scala.language.postfixOps
+    Constraint("constraint.withinTwoYears") {
+      case dmy@models.DayMonthYear(_, _, _, _, _) if dmy.`yyyy-MM-dd` >= (dateService.today - 2 years).`yyyy-MM-dd` => Valid
+      case _ => Invalid(ValidationError("error.withinTwoYears"))
+    }
   }
 }
---
+++ app/services/DateService.scala
@@ -0,0 +1,5 @@
+package services
+
+trait DateService {
+  def today: models.DayMonthYear
+}
