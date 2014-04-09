13f22bc7a08072bb3c656cd05016a0cea934e4ff
Scott Walker <scott.walker@valtech.co.uk>
2014-04-09 11:09:45 +0100

    Removed unused fake implementation yay!
    Former-commit-id: ccee4e643a534f14feb81180c656dd67c2a03833

--- app/services/fakes/FakeAddressLookupService.scala
+++ app/services/fakes/FakeAddressLookupService.scala
@@ -7,22 +7,6 @@ import javax.inject.Inject
 import services.address_lookup.{AddressLookupWebService, AddressLookupService}
 import FakeWebServiceImpl.{traderUprnValid, traderUprnValid2}
 
-/**
- * Fake implementation of the FakeAddressLookupService trait
- */
-class FakeAddressLookupService @Inject()(ws: AddressLookupWebService) extends AddressLookupService {
-  // TODO I think this class should no longer exist and we should have the real service call the fake webservice.
-  override def fetchAddressesForPostcode(postcode: String): Future[Seq[(String, String)]] = Future {
-    if (postcode == FakeAddressLookupService.postcodeInvalid) Seq.empty
-    else FakeAddressLookupService.fetchedAddresses
-  }
-
-  override def fetchAddressForUprn(uprn: String): Future[Option[AddressViewModel]] = Future {
-    if (uprn == FakeWebServiceImpl.traderUprnValid.toString) None
-    else Some(FakeAddressLookupService.address1)
-  }
-}
-
 object FakeAddressLookupService {
   val postcodeInvalid = "xx99xx"
   val address1 = AddressViewModel(uprn = Some(traderUprnValid), address = Seq("44 Hythe Road", "White City", "London", "NW10 6RJ"))
