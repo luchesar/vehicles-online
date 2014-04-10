9d339a760a6d834fcfcab23d1547b07472752c98
Scott Walker <scott.walker@valtech.co.uk>
2014-04-10 09:06:53 +0100

    Refactor: BusinessChooseYourAddressPage using addressSelectId, removed unused vehicleLookupKey
    Former-commit-id: 021f45b0374ed6e9099a114684f4d69d3e075698

--- test/helpers/disposal_of_vehicle/Helper.scala
+++ test/helpers/disposal_of_vehicle/Helper.scala
@@ -1,14 +1,9 @@
 package helpers.disposal_of_vehicle
 
-import models.domain.disposal_of_vehicle.AddressViewModel
-import services.fakes.FakeWebServiceImpl.traderUprnValid
-import services.fakes.FakeVehicleLookupWebService._
 import scala.annotation.tailrec
 
 object Helper {
   val traderBusinessNameValid = "example trader name"
-
-  val vehicleLookupKey = referenceNumberValid + "." + registrationNumberValid
 
   def countSubstring(str1:String, str2:String):Int={
     @tailrec def count(pos:Int, c:Int):Int={
