ac93f9f20f8d02beb23c26b124f7ca0451815235
Scott Walker <scott.walker@valtech.co.uk>
2014-04-10 16:08:31 +0100

    Refactor: adding ids for back and submit buttons
    Former-commit-id: 72ff2ea47db11ff590ff402de5b20aa591622029

--- test/pages/disposal_of_vehicle/VehicleLookupFailurePage.scala
+++ test/pages/disposal_of_vehicle/VehicleLookupFailurePage.scala
@@ -2,13 +2,14 @@ package pages.disposal_of_vehicle
 
 import org.openqa.selenium.WebDriver
 import helpers.webbrowser._
+import mappings.disposal_of_vehicle.VehicleLookupFailure._
 
 object VehicleLookupFailurePage extends Page with WebBrowserDSL {
   val address = "/disposal-of-vehicle/vehicle-lookup-failure"
   override val url: String = WebDriverFactory.testUrl + address.substring(1)
   override val title: String = "Dispose a vehicle into the motor trade: vehicle lookup failure"
 
-  def setupTradeDetails(implicit driver: WebDriver): Element = find(id("setuptradedetails")).get
+  def setupTradeDetails(implicit driver: WebDriver): Element = find(id(setuptradedetailsId)).get
 
-  def vehicleLookup(implicit driver: WebDriver): Element = find(id("vehiclelookup")).get
+  def vehicleLookup(implicit driver: WebDriver): Element = find(id(vehiclelookupId)).get
 }
