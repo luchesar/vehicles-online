837423e88fe4860eb89eb134a1444f6c99fd11bc
Scott Walker <scott.walker@valtech.co.uk>
2014-04-10 13:06:40 +0100

    Refactor: default valid address lines should not be using hypthens - these are tested in separate tests
    Former-commit-id: 83f8685ae96902e8050fdec6067ff5aabf03ce30

--- app/services/fakes/FakeAddressLookupService.scala
+++ app/services/fakes/FakeAddressLookupService.scala
@@ -12,9 +12,9 @@ object FakeAddressLookupService {
   val addressWithoutUprn = AddressViewModel(address= Seq("44 Hythe Road", "White City", "London", "NW10 6RJ"))
   val addressWithUprn = AddressViewModel(uprn = Some(traderUprnValid), address = Seq("44 Hythe Road", "White City", "London", "NW10 6RJ"))
   val line1Valid = "123"
-  val line2Valid = "line-2 stub"
-  val line3Valid = "line-3 stub"
-  val line4Valid = "line-4 stub"
+  val line2Valid = "line2 stub"
+  val line3Valid = "line3 stub"
+  val line4Valid = "line4 stub"
   val postcodeValid = "CM81QJ"
   val postcodeValidWithSpace = "CM8 1QJ"
   val postcodeNoResults = "SA99 1DD"
