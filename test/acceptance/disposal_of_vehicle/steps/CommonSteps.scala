0d817eeee1fc101955e7f7fed994fc8361e13214
Sion Dafydd <sion.dafydd@valtech.co.uk>
2014-04-15 13:16:31 +0100

    Write cucumber acceptance test(s) for US74
    Former-commit-id: 3d1946fac00e39c97ffe961b67c35a90b8dd0ece

--- test/acceptance/disposal_of_vehicle/steps/CommonSteps.scala
+++ test/acceptance/disposal_of_vehicle/steps/CommonSteps.scala
@@ -17,40 +17,6 @@ class CommonSteps(webBrowserDriver:WebBrowserDriver) extends WebBrowserDSL with 
 
   implicit val webDriver = webBrowserDriver.asInstanceOf[WebDriver]
 
-  @Given("""^that the user has entered all required information$""")
-  def that_the_user_has_entered_all_required_information() = {
-    go to BeforeYouStartPage
-    click on BeforeYouStartPage.startNow
-    SetupTradeDetailsPage.dealerName enter traderBusinessNameValid
-    SetupTradeDetailsPage.dealerPostcode enter postcodeValid
-    click on SetupTradeDetailsPage.lookup
-    BusinessChooseYourAddressPage.chooseAddress.value = traderUprnValid.toString
-    click on BusinessChooseYourAddressPage.select
-    VehicleLookupPage.vehicleRegistrationNumber enter registrationNumberValid
-    VehicleLookupPage.documentReferenceNumber enter referenceNumberValid
-    click on VehicleLookupPage.findVehicleDetails
-  }
-
-  @When("""^the summary page has been reached$""")
-  def the_summary_page_has_been_reached() = {
-    page.title should equal(DisposePage.title)
-  }
-
-  @Then("""^the information entered is presented back to the motor trader$""")
-  def the_information_entered_is_presented_back_to_the_motor_trader() = {
-    page.text should include(traderBusinessNameValid)
-    page.text should include(registrationNumberValid)
-    page.text should include("presentationProperty stub")
-    page.text should include("property stub")
-    page.text should include("street stub")
-    page.text should include("town stub")
-    page.text should include("area stub")
-    page.text should include(postcodeValid)
-    DisposePage.consent.isDisplayed should equal(true)
-    DisposePage.lossOfRegistrationConsent.isDisplayed should equal(true)
-    DisposePage.mileage.isDisplayed should equal(true)
-  }
-
   @Then("""^a single error message is displayed "(.*)"$""")
   def a_single_error_message_is_displayed(message:String) = {
     ErrorPanel.numberOfErrors should equal(1)
