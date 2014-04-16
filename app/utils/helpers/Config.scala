806f10d6d01009d56d01eb7729ef21232490ff45
Ian Stainer <ian.stainer@valtech.co.uk>
2014-04-16 17:19:11 +0100

    Changes to support the split out vehicle lookup ms
    Former-commit-id: 5ff68e5696bd5819db765a7192e4fba4ef2e7e2e

--- app/utils/helpers/Config.scala
+++ app/utils/helpers/Config.scala
@@ -6,7 +6,8 @@ import scala.language.postfixOps
 
 object Config {
   // Micro-service config
-  val microServiceBaseUrl = getProperty("microServiceUrlBase", "http://localhost:8080")
+  val vehicleLookupMicroServiceBaseUrl = getProperty("vehicleLookupMicroServiceUrlBase", "http://localhost:8090")
+  val microServiceBaseUrl = getProperty("disposeVehicleMicroServiceUrlBase", "http://localhost:8080")
 
   // Ordnance survey config
   val ordnanceSurveyUsername = getProperty("ordnancesurvey.username", "")
