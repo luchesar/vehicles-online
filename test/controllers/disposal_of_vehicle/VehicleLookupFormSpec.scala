d09c766df4e2c078395554dab0d8095b99cc9230
Matthew Snaith <fault_line@hotmail.co.uk>
2014-04-22 13:29:57 +0100

    refactored VehicleLookupFormSpec to ease readability, created ValidVRMFormat and InvalidVRMFormat objects to allow formats to be easily changed
    Former-commit-id: 3950f45e3fdb35ca07579c4700eff1c6801f78ad

--- test/controllers/disposal_of_vehicle/VehicleLookupFormSpec.scala
+++ test/controllers/disposal_of_vehicle/VehicleLookupFormSpec.scala
@@ -12,8 +12,83 @@ import scala.concurrent.{ExecutionContext, Future}
 import play.api.libs.json.Json
 import ExecutionContext.Implicits.global
 import services.fakes.FakeVehicleLookupWebService._
+import helpers.disposal_of_vehicle.InvalidVRMFormat._
+import helpers.disposal_of_vehicle.ValidVRMFormat._
 
 class VehicleLookupFormSpec extends UnitSpec {
+
+  "form" should {
+    "accept when all fields contain valid responses" in {
+      validVehicleLookupForm().get.referenceNumber should equal(referenceNumberValid)
+      validVehicleLookupForm().get.registrationNumber should equal(registrationNumberValid)
+    }
+  }
+
+  "referenceNumber" should {
+    allInvalidVrmFormats.map(vrm => "reject invalid vehicle registration mark : " + vrm in {
+      validVehicleLookupForm(registrationNumber = vrm).errors should have length 1
+    })
+
+    allValidVrmFormats.map(vrm => "accept valid vehicle registration mark : " + vrm in {
+      validVehicleLookupForm(registrationNumber = vrm).get.registrationNumber should equal(vrm)
+    })
+
+    "reject if blank" in {
+      val vehicleLookupFormError = validVehicleLookupForm(referenceNumber = "").errors
+      val expectedKey = referenceNumberId
+
+      vehicleLookupFormError should have length 3
+      vehicleLookupFormError(0).key should equal(expectedKey)
+      vehicleLookupFormError(0).message should equal("error.minLength")
+      vehicleLookupFormError(1).key should equal(expectedKey)
+      vehicleLookupFormError(1).message should equal("error.required")
+      vehicleLookupFormError(2).key should equal(expectedKey)
+      vehicleLookupFormError(2).message should equal("error.restricted.validNumberOnly")
+    }
+
+    "reject if less than min length" in {
+      validVehicleLookupForm(referenceNumber = "1234567891").errors should have length 1
+    }
+
+    "reject if greater than max length" in {
+      validVehicleLookupForm(referenceNumber = "123456789101").errors should have length 1
+    }
+
+    "reject if contains letters" in {
+      validVehicleLookupForm(referenceNumber = "qwertyuiopl").errors should have length 1
+    }
+
+    "reject if contains special characters" in {
+      validVehicleLookupForm(referenceNumber = "£££££££££££").errors should have length 1
+    }
+
+    "accept if valid" in {
+      validVehicleLookupForm(registrationNumber = registrationNumberValid).get.referenceNumber should equal(referenceNumberValid)
+    }
+  }
+
+  "registrationNumber" should {
+    "reject if empty" in {
+      validVehicleLookupForm(registrationNumber = "").errors should have length 3
+    }
+
+    "reject if less than min length" in {
+      validVehicleLookupForm(registrationNumber = "a").errors should have length 2
+    }
+
+    "reject if more than max length" in {
+      validVehicleLookupForm(registrationNumber = "AB53WERT").errors should have length 1
+    }
+
+    "reject if more than max length 2" in {
+      validVehicleLookupForm(registrationNumber = "PJ056YYY").errors should have length 1
+    }
+
+    "reject if contains special characters" in {
+      validVehicleLookupForm(registrationNumber = "ab53ab%").errors should have length 1
+    }
+  }
+
   private val vehicleLookup = {
     val ws: VehicleLookupWebService = mock[VehicleLookupWebService]
     when(ws.callVehicleLookupService(any[VehicleDetailsRequest])).thenReturn(Future {
@@ -25,7 +100,7 @@ class VehicleLookupFormSpec extends UnitSpec {
     new disposal_of_vehicle.VehicleLookup(vehicleLookupServiceImpl)
   }
 
-  private def formWithValidDefaults(referenceNumber: String = referenceNumberValid,
+  private def validVehicleLookupForm(referenceNumber: String = referenceNumberValid,
                                     registrationNumber: String = registrationNumberValid,
                                     consent: String = consentValid) = {
     vehicleLookup.vehicleLookupForm.bind(
@@ -36,77 +111,5 @@ class VehicleLookupFormSpec extends UnitSpec {
     )
   }
 
-  "form" should {
-    "accept when all fields contain valid responses" in {
-      val model = formWithValidDefaults().get
-      model.referenceNumber should equal(referenceNumberValid)
-      model.registrationNumber should equal(registrationNumberValid)
-    }
-  }
 
-  "referenceNumber" should {
-    val allInvalidVrmFormats = Seq("9A99A99", "9A9A99", "9AAAA9", "9A99AA9", "99AAA99", "99AAAA9", "A9AAA99", "A9A9A9A", "A9A9A99", "A9AAAA9", "99A999", "A9A9AAA", "99A9A9", "9AA9AA9", "AAA9A99", "999A999", "AAA9AA9", "A9AA99", "A99A999", "A999A9", "9AA999", "A99999A", "A99A9AA", "99A99", "9AAA9", "A99A9", "A99A9A9", "9A9999A", "99AA9A", "9A999A", "9A99999", "9AA9A9", "A9999AA", "99AA9", "9A999AA", "999A9A", "99AAAA", "99AA99A", "A99A", "AA9AA9A", "AA999AA", "AAAA9A", "AA9AAAA", "AA9A9A", "AAAAAA", "A9A999A", "A9AA9AA", "A9A99AA", "9AAAA9A", "9AA99AA", "999AA9", "999AA99", "999AAA9", "A9A99A", "A99AA99", "A999A99", "A99AAA9", "9AA99A9", "AA9AA", "9AAAAA9", "9A9AAA", "9A9AA9", "9999", "99A9A9A", "99AAAAA", "99A9", "AA99AA9", "99A9AAA", "9AA99", "999A9", "99999A", "999A99A", "A99999", "999A9AA", "999999A", "A9AA9A", "AAA9AA", "AAA9A9", "A99A9A", "A9AAAA", "A9AA99A", "9AAA99A", "9A9", "A9A9A", "AAAAA", "9A9AA9A", "9AAA9AA", "9A99AA", "AAAA999", "9999999", "999A9A9", "AAAA9AA", "9A99A", "999A99", "99AAA9", "9A9AA", "9A9AAA9", "AA9AA99", "99A9999", "99AA9A9", "AAAA99", "9999A9A", "A9A9", "AA99A99", "AA9AAA9", "99A99AA", "99A99A9", "AAAAA9", "A9A9999", "A999A9A", "AA99A", "A9A99A9", "9AAAA99", "A9AAAAA", "9AA9A9A", "9AAAAAA", "99", "AAAAA99", "9AA9AAA", "AA9A", "AAA9A9A", "AAAAAA9", "A9A999", "9A9A", "AAAA", "AAA9AAA", "9AA9A", "A999AA", "A99A99A", "9A9A99A", "AA9A9", "9AA99A", "99A9A", "9AAAA", "9A9A9AA", "9AA9AA", "99A9A99", "AA9A99A", "AA9A999", "AA9999A", "AA9A9AA", "99A9AA9", "9AA9", "AA999A", "AA99AA", "A9A9AA9", "A9A", "AAAA99A", "AAA", "99999AA", "99999A9", "A99A99", "A9AAA9", "A999999", "9A9999", "A99AA9", "A9999A9", "9A9AA99", "9A999A9", "9AAA99", "9A99A9", "99AA999", "9A9A9A", "9AAAAA", "AAAA9A9", "9A99AAA", "A9AA", "AA99A9A", "99AAA9A", "AAA99A9", "AA9A99", "A9AAA9A", "AA9AAA", "AA9AA9", "99999", "9A9A9", "99A99A", "99A9AA", "9999A99", "AA", "9999AA9", "A9999A", "A9A9AA", "A9A9A9", "A999A", "A999AA9", "9AA9A99", "999", "A99AA", "9A99", "9A9A999", "999999", "9A9A9A9", "99AA99", "9999A9", "AA99999", "AA9A9A9", "999AA9A", "99AA9AA", "99A999A", "AA999A9", "A9AA999", "AA99A9", "999AAAA", "9AAA999", "A99AA9A", "A9A99", "AAAA9", "A9AA9A9", "9AA999A", "9AA9999", "9AAA9A", "9AAA9A9", "A99AAAA", "9A99A9A", "A9AA9", "9A9AAAA", "AAAAA9A", "AAA99AA", "AAAAAAA", "9A999")
-    allInvalidVrmFormats.map(vrm => "reject any invalid vehicle registration mark : " + vrm in {
-      formWithValidDefaults(registrationNumber = vrm).errors should have length 1
-    })
-
-    val allValidVrmFormats = Seq("A9", "A99", "A999", "A9999", "AA9", "AA99", "AA999", "AA9999", "AAA9", "AAA99", "AAA999", "AAA9999", "AAA9A", "AAA99A", "AAA999A", "9A", "9AA", "9AAA", "99A", "99AA", "99AAA", "999A", "999AA", "999AAA", "9999A", "9999AA", "A9AAA", "A99AAA", "A999AAA","AA99AAA", "9999AAA")
-    allValidVrmFormats.map(vrm => "accept any valid vehicle registration mark : " + vrm in {
-      formWithValidDefaults(registrationNumber = vrm).get.registrationNumber should equal(vrm)
-    })
-
-    "reject if blank" in {
-      val errors = formWithValidDefaults(referenceNumber = "").errors
-      errors should have length 3
-      val expectedKey = referenceNumberId
-      errors(0).key should equal(expectedKey)
-      errors(0).message should equal("error.minLength")
-      errors(1).key should equal(expectedKey)
-      errors(1).message should equal("error.required")
-      errors(2).key should equal(expectedKey)
-      errors(2).message should equal("error.restricted.validNumberOnly")
-    }
-
-    "reject if less than min length" in {
-      formWithValidDefaults(referenceNumber = "1234567891").errors should have length 1
-    }
-
-    "reject if greater than max length" in {
-      formWithValidDefaults(referenceNumber = "123456789101").errors should have length 1
-    }
-
-    "reject if contains letters" in {
-      formWithValidDefaults(referenceNumber = "qwertyuiopl").errors should have length 1
-    }
-
-    "reject if contains special characters" in {
-      formWithValidDefaults(referenceNumber = "£££££££££££").errors should have length 1
-    }
-
-    "accept if valid" in {
-      formWithValidDefaults(registrationNumber = registrationNumberValid).get.referenceNumber should equal(referenceNumberValid)
-    }
-  }
-
-  "registrationNumber" should {
-    "reject if empty" in {
-      formWithValidDefaults(registrationNumber = "").errors should have length 3
-    }
-
-    "reject if less than min length" in {
-      formWithValidDefaults(registrationNumber = "a").errors should have length 2
-    }
-
-    "reject if more than max length" in {
-      formWithValidDefaults(registrationNumber = "AB53WERT").errors should have length 1
-    }
-
-    "reject if more than max length 2" in {
-      formWithValidDefaults(registrationNumber = "PJ056YYY").errors should have length 1
-    }
-
-    "reject if contains special characters" in {
-      formWithValidDefaults(registrationNumber = "ab53ab%").errors should have length 1
-    }
-  }
 }
---
+++ test/helpers/disposal_of_vehicle/ValidVRMFormat.scala
@@ -0,0 +1,37 @@
+package helpers.disposal_of_vehicle
+
+object ValidVRMFormat {
+  val allValidVrmFormats = Seq(
+    "A9",
+    "A99",
+    "A999",
+    "A9999",
+    "AA9",
+    "AA99",
+    "AA999",
+    "AA9999",
+    "AAA9",
+    "AAA99",
+    "AAA999",
+    "AAA9999",
+    "AAA9A",
+    "AAA99A",
+    "AAA999A",
+    "9A",
+    "9AA",
+    "9AAA",
+    "99A",
+    "99AA",
+    "99AAA",
+    "999A",
+    "999AA",
+    "999AAA",
+    "9999A",
+    "9999AA",
+    "A9AAA",
+    "A99AAA",
+    "A999AAA",
+    "AA99AAA",
+    "9999AAA"
+  )
+}
