9cef1963cac4e1b224f93561a0d80f24b984ec86
Ian Stainer <ian.stainer@valtech.co.uk>
2014-05-08 13:09:22 +0100

    The first page now removes all the cookies that may be left over from a previous run through the pages
    Former-commit-id: e520b66160d1439d0d9e4e739d83bf4d83e376ca

--- app/controllers/disposal_of_vehicle/BeforeYouStart.scala
+++ app/controllers/disposal_of_vehicle/BeforeYouStart.scala
@@ -2,13 +2,31 @@ package controllers.disposal_of_vehicle
 
 import play.api.mvc._
 import play.api.Logger
+import mappings.disposal_of_vehicle.Dispose._
+import mappings.disposal_of_vehicle.VehicleLookup._
+import mappings.disposal_of_vehicle.SetupTradeDetails._
+import mappings.disposal_of_vehicle.DealerDetails._
+import mappings.disposal_of_vehicle.BusinessChooseYourAddress._
+import utils.helpers.CryptoHelper
 
 class BeforeYouStart extends Controller {
 
   def present = Action { implicit request =>
-    val uniqueId = java.util.UUID.randomUUID.toString
-    Logger.debug(s"BeforeYouStart - storing the following in session: modelId = $uniqueId")
-    Ok(views.html.disposal_of_vehicle.before_you_start()).withSession("modelId" -> uniqueId)
+
+    Ok(views.html.disposal_of_vehicle.before_you_start()).withNewSession.
+      discardingCookies(
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(SetupTradeDetailsCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(dealerDetailsCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(businessChooseYourAddressCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(vehicleLookupDetailsCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(vehicleLookupResponseCodeCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(vehicleLookupFormModelCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(disposeFormModelCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(disposeFormTransactionIdCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(disposeFormTimestampIdCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(disposeFormRegistrationNumberCacheKey)),
+        DiscardingCookie(name = CryptoHelper.encryptCookieName(disposeModelCacheKey))
+      )
   }
 
   def submit = Action { implicit request =>
