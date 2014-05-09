a8f6e0472f66f9262f36de63224ede48666a9a3d
Scott Walker <scott.walker@valtech.co.uk>
2014-05-09 10:32:32 +0100

    US137 code re-use in cookie factory
    Former-commit-id: 42ffd6b938a271b09f8b4c0afe43d1209ebd9e5a

--- test/helpers/disposal_of_vehicle/CookieFactory.scala
+++ test/helpers/disposal_of_vehicle/CookieFactory.scala
@@ -3,96 +3,90 @@ package helpers.disposal_of_vehicle
 import services.fakes.FakeAddressLookupService._
 import models.domain.disposal_of_vehicle._
 import helpers.disposal_of_vehicle.Helper._
-import play.api.libs.json.Json
+import play.api.libs.json.{Writes, Json}
 import services.fakes.FakeVehicleLookupWebService._
 import services.fakes.{FakeDisposeWebServiceImpl, FakeVehicleLookupWebService}
 import models.DayMonthYear
 import mappings.disposal_of_vehicle.Dispose._
 import services.fakes.FakeDateServiceImpl._
 import services.fakes.FakeDisposeWebServiceImpl._
+import play.api.mvc.Cookie
+import mappings.disposal_of_vehicle.SetupTradeDetails.SetupTradeDetailsCacheKey
+import mappings.disposal_of_vehicle.TraderDetails.traderDetailsCacheKey
+import mappings.disposal_of_vehicle.VehicleLookup.vehicleLookupFormModelCacheKey
+import mappings.disposal_of_vehicle.VehicleLookup.vehicleLookupDetailsCacheKey
+import mappings.disposal_of_vehicle.Dispose.disposeFormModelCacheKey
+import mappings.disposal_of_vehicle.Dispose.disposeFormRegistrationNumberCacheKey
 
 object CookieFactory { // TODO setup the cookies for the Unit Specs here, removing them from CacheSetup
+  private def createCookie[A](key: String, value: A)(implicit tjs: Writes[A]): Cookie = {
+    val valueAsString = Json.toJson(value).toString()
+    Cookie(key, valueAsString)
+  }
+
   def setupTradeDetails(traderPostcode: String = postcodeValid) = {
-    val key = mappings.disposal_of_vehicle.SetupTradeDetails.SetupTradeDetailsCacheKey
+    val key = SetupTradeDetailsCacheKey
     val value = SetupTradeDetailsModel(traderBusinessName = traderBusinessNameValid,
       traderPostcode = traderPostcode)
-    val valueAsString = Json.toJson(value).toString()
-    play.api.mvc.Cookie(key, valueAsString)
+    createCookie(key, value)
   }
 
-  def dealerDetails(uprn: Option[Long] = None, line1: String = "my house", traderPostcode: String = postcodeValid) = {
-    val key = mappings.disposal_of_vehicle.TraderDetails.traderDetailsCacheKey
-    val value = TraderDetailsModel(traderName = traderBusinessNameValid,
-      traderAddress = AddressViewModel(uprn = uprn, address = Seq(line1, "my street", "my area", "my town", "CM81QJ")))
-    val valueAsString = Json.toJson(value).toString()
-    play.api.mvc.Cookie(key, valueAsString)
-  }
+    def dealerDetails(uprn: Option[Long] = None, line1: String = "my house", traderPostcode: String = postcodeValid) = {
+      val key = traderDetailsCacheKey
+      val value = TraderDetailsModel(traderName = traderBusinessNameValid,
+        traderAddress = AddressViewModel(uprn = uprn, address = Seq(line1, "my street", "my area", "my town", "CM81QJ")))
+      createCookie(key, value)
+    }
 
-  def vehicleLookupFormModel(referenceNumber: String = referenceNumberValid,
-                             registrationNumber: String = registrationNumberValid) = {
-    val key = mappings.disposal_of_vehicle.VehicleLookup.vehicleLookupFormModelCacheKey
-    val value = VehicleLookupFormModel(referenceNumber = referenceNumber,
-      registrationNumber = registrationNumber)
-    val valueAsString = Json.toJson(value).toString()
-    play.api.mvc.Cookie(key, valueAsString)
-  }
+    def vehicleLookupFormModel(referenceNumber: String = referenceNumberValid,
+                               registrationNumber: String = registrationNumberValid) = {
+      val key = vehicleLookupFormModelCacheKey
+      val value = VehicleLookupFormModel(referenceNumber = referenceNumber,
+        registrationNumber = registrationNumber)
+      createCookie(key, value)
+    }
 
-  def vehicleDetailsModel(registrationNumber: String = registrationNumberValid,
-                          vehicleMake: String = FakeVehicleLookupWebService.vehicleMakeValid,
-                          vehicleModel: String = vehicleModelValid,
-                          keeperName: String = keeperNameValid) = {
-    val key = mappings.disposal_of_vehicle.VehicleLookup.vehicleLookupDetailsCacheKey
-    val value = VehicleDetailsModel(registrationNumber = registrationNumber,
-      vehicleMake = vehicleMake,
-      vehicleModel = vehicleModel)
-    val valueAsString = Json.toJson(value).toString()
-    play.api.mvc.Cookie(key, valueAsString)
-  }
+     def vehicleDetailsModel(registrationNumber: String = registrationNumberValid,
+                             vehicleMake: String = FakeVehicleLookupWebService.vehicleMakeValid,
+                             vehicleModel: String = vehicleModelValid,
+                             keeperName: String = keeperNameValid) = {
+       val key = vehicleLookupDetailsCacheKey
+       val value = VehicleDetailsModel(registrationNumber = registrationNumber,
+         vehicleMake = vehicleMake,
+         vehicleModel = vehicleModel)
+       createCookie(key, value)
+     }
 
-  def disposeFormModel() = {
-    val key = mappings.disposal_of_vehicle.Dispose.disposeFormModelCacheKey
-    val value = DisposeFormModel(mileage = None,
-      dateOfDisposal = DayMonthYear.today,
-      consent = FakeDisposeWebServiceImpl.consentValid,
-      lossOfRegistrationConsent = FakeDisposeWebServiceImpl.consentValid)
-    val valueAsString = Json.toJson(value).toString()
-    play.api.mvc.Cookie(key, valueAsString)
-  }
+     def disposeFormModel() = {
+       val key = disposeFormModelCacheKey
+       val value = DisposeFormModel(mileage = None,
+         dateOfDisposal = DayMonthYear.today,
+         consent = FakeDisposeWebServiceImpl.consentValid,
+         lossOfRegistrationConsent = FakeDisposeWebServiceImpl.consentValid)
+       createCookie(key, value)
+     }
 
-  def disposeFormRegistrationNumber(registrationNumber: String = registrationNumberValid) = {
-    val key = disposeFormRegistrationNumberCacheKey
-    val value = registrationNumber
-    play.api.mvc.Cookie(key, value)
-  }
+     def disposeFormRegistrationNumber(registrationNumber: String = registrationNumberValid) =
+       Cookie(disposeFormRegistrationNumberCacheKey, registrationNumber)
 
-  def disposeFormTimestamp(timestamp: String = s"$dateOfDisposalYearValid-$dateOfDisposalMonthValid-${dateOfDisposalDayValid}T00:00:00.000+01:00") = {
-    val key = disposeFormTimestampIdCacheKey
-    val value = timestamp
-    play.api.mvc.Cookie(key, value)
-  }
+     def disposeFormTimestamp(timestamp: String = s"$dateOfDisposalYearValid-$dateOfDisposalMonthValid-${dateOfDisposalDayValid}T00:00:00.000+01:00") =
+       Cookie(disposeFormTimestampIdCacheKey, timestamp)
 
-  def disposeTransactionId(transactionId: String = transactionIdValid) = {
-    val key = disposeFormTransactionIdCacheKey
-    val value = transactionId
-    play.api.mvc.Cookie(key, value)
-  }
+     def disposeTransactionId(transactionId: String = transactionIdValid) =
+       Cookie(disposeFormTransactionIdCacheKey, transactionId)
 
-  def vehicleRegistrationNumber(registrationNumber: String = registrationNumberValid) = {
-    val key = mappings.disposal_of_vehicle.Dispose.disposeFormRegistrationNumberCacheKey
-    val value = registrationNumber
-    play.api.mvc.Cookie(key, value)
-  }
+     def vehicleRegistrationNumber(registrationNumber: String = registrationNumberValid) =
+       Cookie(disposeFormRegistrationNumberCacheKey, registrationNumber)
 
-  def disposeModel(referenceNumber: String = referenceNumberValid,
-                   registrationNumber: String = registrationNumberValid,
-                   dateOfDisposal: DayMonthYear = DayMonthYear.today,
-                   mileage: Option[Int] = None) = {
-    val key = mappings.disposal_of_vehicle.Dispose.disposeModelCacheKey
-    val value = DisposeModel(referenceNumber = referenceNumber,
-      registrationNumber = registrationNumber,
-      dateOfDisposal = dateOfDisposal,
-      mileage = mileage)
-    val valueAsString = Json.toJson(value).toString()
-    play.api.mvc.Cookie(key, valueAsString)
-  }
+     def disposeModel(referenceNumber: String = referenceNumberValid,
+                      registrationNumber: String = registrationNumberValid,
+                      dateOfDisposal: DayMonthYear = DayMonthYear.today,
+                      mileage: Option[Int] = None) = {
+       val key = mappings.disposal_of_vehicle.Dispose.disposeModelCacheKey
+       val value = DisposeModel(referenceNumber = referenceNumber,
+         registrationNumber = registrationNumber,
+         dateOfDisposal = dateOfDisposal,
+         mileage = mileage)
+       createCookie(key, value)
+     }
 }
