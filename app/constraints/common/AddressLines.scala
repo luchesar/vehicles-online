6e74100f0af1f932b0635ccd18f0640ea7321b8f
Ian Stainer <ian.stainer@valtech.co.uk>
2014-05-09 17:10:25 +0100

    Code tidy up
    Former-commit-id: ae2e2f2328bcb4e7cbb322e4c513261226a35192

--- app/constraints/common/AddressLines.scala
+++ app/constraints/common/AddressLines.scala
@@ -6,13 +6,12 @@ import models.domain.common.AddressLinesModel
 
 object AddressLines {
   def validAddressLines: Constraint[AddressLinesModel] = Constraint[AddressLinesModel]("constraint.required") {
-    case input@AddressLinesModel(line1, _, _, _) => {
+    case input@AddressLinesModel(line1, _, _, _) =>
       // Reject when: Too many characters, HTML or XML markup. Regex states string must contain at least one number or letter, can also include punctuation
       val whitelist = """^(?=.*[a-zA-Z0-9])[A-Za-z0-9\s\-\,\.\/\\]*$""".r
       if (input.totalCharacters >= maxLengthOfLinesConcatenated) Invalid(ValidationError("error.address.maxLengthOfLinesConcatenated"))
       else if (!whitelist.pattern.matcher(input.toViewFormat.mkString).matches) Invalid(ValidationError("error.address.characterinvalid"))
       else Valid
-    }
     case _ => Invalid(ValidationError("error.address.line1Required"))
   }
 }
