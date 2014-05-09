7f511e792d7eef5c77d7708e54691850561cf491
Scott Walker <scott.walker@valtech.co.uk>
2014-05-09 15:41:34 +0100

    Refactor: remove unused submit from DisposeFailure
    Former-commit-id: bbf47f812acf157a6e6637c5e5300b602dac0c16

--- test/controllers/disposal_of_vehicle/DisposeFailureUnitSpec.scala
+++ test/controllers/disposal_of_vehicle/DisposeFailureUnitSpec.scala
@@ -2,47 +2,22 @@ package controllers.disposal_of_vehicle
 
 import play.api.test.{FakeRequest, WithApplication}
 import play.api.test.Helpers._
-import pages.disposal_of_vehicle._
 import helpers.disposal_of_vehicle.CookieFactoryForUnitSpecs
 import helpers.UnitSpec
 
 class DisposeFailureUnitSpec extends UnitSpec {
-
-  "DisposalFailure - Controller" should {
-    "present" in new WithApplication {
+  "present" should {
+    "display the page" in new WithApplication {
       val request = FakeRequest().withSession().
         withCookies(CookieFactoryForUnitSpecs.traderDetailsModel()).
         withCookies(CookieFactoryForUnitSpecs.vehicleDetailsModel()).
         withCookies(CookieFactoryForUnitSpecs.disposeFormModel()).
         withCookies(CookieFactoryForUnitSpecs.disposeTransactionId())
-      val result = disposeFailure().present(request)
+      val result = new DisposeFailure().present(request)
       whenReady(result) {
         r => r.header.status should equal(OK)
       }
     }
-
-    "redirect to vehicle lookup page when button clicked" in new WithApplication {
-      val request = FakeRequest().withSession().
-        withCookies(CookieFactoryForUnitSpecs.traderDetailsModel()).
-        withCookies(CookieFactoryForUnitSpecs.vehicleDetailsModel()).
-        withCookies(CookieFactoryForUnitSpecs.disposeFormModel()).
-        withCookies(CookieFactoryForUnitSpecs.disposeTransactionId())
-      val result = disposeFailure().submit(request)
-      whenReady(result) {
-        r => r.header.headers.get(LOCATION) should equal(Some(VehicleLookupPage.address))
-      }
-    }
-
-    "redirect to setuptraderdetails when no details are in cache and submit is selected" in new WithApplication() {
-      val request = FakeRequest().withSession()
-      val result = disposeFailure().submit(request)
-      whenReady(result) {
-        r => r.header.headers.get(LOCATION) should equal(Some(SetupTradeDetailsPage.address))
-      }
-    }
   }
-
-  private def disposeFailure() =
-    new DisposeFailure()
 }
 
