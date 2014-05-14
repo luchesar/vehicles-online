ec56a86655c2906a79a99439f2a276146040d1ec
Matthew Snaith <fault_line@hotmail.co.uk>
2014-05-14 11:28:09 +0100

    Changes ot US134, manual address rules amended to 30 characters per line and max concatinated lines of 120 characters. all appropriate tests amended
    Former-commit-id: dc90a0c53f63566a2c749634603d6d3ffb17d34f

--- app/mappings/common/AddressLines.scala
+++ app/mappings/common/AddressLines.scala
@@ -2,7 +2,6 @@ package mappings.common
 
 import play.api.data.Mapping
 import play.api.data.Forms._
-import play.api.data.validation.Invalid
 import models.domain.common.AddressLinesModel
 
 object AddressLines {
@@ -12,13 +11,12 @@ object AddressLines {
   val line3Id = "line3"
   val line4Id = "line4"
   val line1MinLength = 1
-  val line1MaxLength = 50
-  val lineMaxLength = 75
-  val maxLengthOfLinesConcatenated = 130
+  val lineMaxLength = 30
+  val maxLengthOfLinesConcatenated = 120
   val addressLinesCacheKey = "addressLines"
 
   def addressLines: Mapping[AddressLinesModel] = mapping(
-    line1Id -> nonEmptyText(minLength = line1MinLength, maxLength = line1MaxLength),
+    line1Id -> nonEmptyText(minLength = line1MinLength, maxLength = lineMaxLength),
     line2Id -> optional(text(maxLength = lineMaxLength)),
     line3Id -> optional(text(maxLength = lineMaxLength)),
     line4Id -> optional(text(maxLength = lineMaxLength))
