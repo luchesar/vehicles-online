cef2831d3a7684b1a196f8b3ef78cd6f989fc99c
Scott Walker <scott.walker@valtech.co.uk>
2014-05-16 16:32:41 +0100

    Refactor: test for AddressLinesModel totalCharacters
    Former-commit-id: a5dadfc17a8f7dfee3c6df306c0b20980882b66e

--- app/models/domain/common/AddressLinesModel.scala
+++ app/models/domain/common/AddressLinesModel.scala
@@ -1,14 +1,14 @@
 package models.domain.common
 
-import play.api.libs.json.Json
 import mappings.common.AddressLines._
+import play.api.libs.json.Json
 
 case class AddressLinesModel(line1: String,
                              line2: Option[String] = None,
                              line3: Option[String] = None,
-                             line4: Option[String] = None){
-
+                             line4: Option[String] = None) {
   def toViewFormat: Seq[String] = Seq(Some(line1), line2, line3, line4).flatten
+
   def totalCharacters = toViewFormat.map(_.length).sum // TODO test this in isolation in a model test
 }
 
