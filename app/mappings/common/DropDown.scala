081664f58c401e8f50ac9f9eb8e641df57db1e40
Scott Walker <scott.walker@valtech.co.uk>
2014-05-19 09:04:16 +0100

    Refactor: removed TODO and dropdown maxlength as it is not relevant now that dropdown is a long rather than string
    Former-commit-id: 0f1cbcda862bdd9efd629c7b6d1df0e7736d551a

--- app/mappings/common/DropDown.scala
+++ app/mappings/common/DropDown.scala
@@ -5,8 +5,6 @@ import play.api.data.Forms._
 import constraints.common.DropDown.validDropDown
 
 object DropDown {
-  val maxLength = 9999 // TODO find out from BAs the maxLength for address in GDS DB
-
   def dropDown: Mapping[Long] = longNumber
 
 /*  def dropDown(dropDownOptions: Map[String, String]): Mapping[String] = {
