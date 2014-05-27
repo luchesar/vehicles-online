9cef8f4e0578e3b585a2aea2f3a9ac17e8ea49ae
Scott Walker <scott.walker@valtech.co.uk>
2014-05-27 17:21:50 +0100

    Refactor: extra tests for AddressViewModelSpec, removed 2 unneccessary tests, added TODO
    Former-commit-id: b3af962cd60efe7aa589b9871abb285538c4d85d

--- test/models/domain/disposal_of_vehicle/AddressViewModelSpec.scala
+++ test/models/domain/disposal_of_vehicle/AddressViewModelSpec.scala
@@ -2,17 +2,39 @@ package models.domain.disposal_of_vehicle
 
 import services.fakes.FakeVehicleLookupWebService._
 import helpers.UnitSpec
+import models.domain.common.{AddressLinesModel, AddressAndPostcodeModel}
+import services.fakes.FakeAddressLookupService._
+import play.api.libs.json.Json
+import AddressViewModel.JsonFormat
 
 final class AddressViewModelSpec extends UnitSpec {
+  "from" should {
+    "translate correctly" in {
+      val addressAndPostcodeModel = AddressAndPostcodeModel(addressLinesModel = AddressLinesModel(line1 = line1Valid,
+        line2 = Some(line2Valid),
+        line3 = Some(line3Valid),
+        line4 = Some(line4Valid)),
+        postcode = postcodeValid)
 
-  "AddressViewModel - model" should {
+      val result = AddressViewModel.from(addressAndPostcodeModel)
 
-    "handle a uprn of the correct size" in {
-      val address = AddressViewModel(uprn = Some(keeperUprnValid), address = Seq("line1", "line2", "line2"))
-
-      val actualUprn = address.uprn.get
-      actualUprn should equal(keeperUprnValid)
-      address.address.size should equal(3)
+      result.uprn should equal(None)
+      result.address should equal(Seq(line1Valid, line2Valid, line3Valid, line4Valid, postcodeValid))
     }
   }
+
+  "format" should {
+    "serialize to json" in {
+      val address = AddressViewModel(uprn = Some(keeperUprnValid), address = Seq(line1Valid, line2Valid, line3Valid, line4Valid, postcodeValid))
+      Json.toJson(address) should equal(asJson)
+    }
+
+    "deserialize from json" in {
+      val fromJson =  Json.fromJson[AddressViewModel](asJson)
+      val expected = AddressViewModel(uprn = Some(keeperUprnValid), address = Seq(line1Valid, line2Valid, line3Valid, line4Valid, postcodeValid))
+      fromJson.asOpt should equal(Some(expected))
+    }
+  }
+
+  private val asJson = Json.parse("""{"uprn":10123456789,"address":["123","line2 stub","line3 stub","line4 stub","CM81QJ"]}""")
 }
