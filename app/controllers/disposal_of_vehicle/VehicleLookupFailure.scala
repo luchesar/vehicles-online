916b87e9feeeadbb6bda7ecae24a531f0984eb7e
Scott Walker <scott.walker@valtech.co.uk>
2014-05-30 17:13:21 +0100

    US270 header on webservice call so that the security service can identify us as the caller and use the correct config, passing info from cookie into vehicle lookup failure
    Former-commit-id: c17b732e92f5f530c46517b30cee05cb328e546d

--- app/controllers/disposal_of_vehicle/VehicleLookupFailure.scala
+++ app/controllers/disposal_of_vehicle/VehicleLookupFailure.scala
@@ -7,7 +7,6 @@ import models.domain.disposal_of_vehicle.{TraderDetailsModel, VehicleLookupFormM
 import mappings.disposal_of_vehicle.VehicleLookup._
 import common.{ClientSideSessionFactory, CookieImplicits}
 import CookieImplicits.RequestCookiesAdapter
-import utils.helpers.{CookieNameHashing, CookieEncryption}
 import models.domain.common.BruteForcePreventionResponse
 import models.domain.common.BruteForcePreventionResponse._
 
@@ -16,7 +15,7 @@ final class VehicleLookupFailure @Inject()()(implicit clientSideSessionFactory: 
   def present = Action { implicit request =>
     (request.cookies.getModel[TraderDetailsModel], request.cookies.getModel[BruteForcePreventionResponse], request.cookies.getModel[VehicleLookupFormModel]) match {
       case (Some(dealerDetails), Some(bruteForcePreventionResponse), Some(vehicleLookUpFormModelDetails)) =>
-        displayVehicleLookupFailure(vehicleLookUpFormModelDetails)
+        displayVehicleLookupFailure(vehicleLookUpFormModelDetails, bruteForcePreventionResponse)
       case _ => Redirect(routes.SetUpTradeDetails.present())
     }
   }
@@ -30,10 +29,15 @@ final class VehicleLookupFailure @Inject()()(implicit clientSideSessionFactory: 
     }
   }
 
-  private def displayVehicleLookupFailure(vehicleLookUpFormModelDetails: VehicleLookupFormModel)(implicit request: Request[AnyContent]) = {
+  private def displayVehicleLookupFailure(vehicleLookUpFormModelDetails: VehicleLookupFormModel, bruteForcePreventionResponse: BruteForcePreventionResponse)(implicit request: Request[AnyContent]) = {
     val responseCodeErrorMessage = encodeResponseCodeErrorMessage
-    Ok(views.html.disposal_of_vehicle.vehicle_lookup_failure(vehicleLookUpFormModelDetails, responseCodeErrorMessage)).
-      discardingCookies(DiscardingCookie(name = VehicleLookupResponseCodeCacheKey))
+    Ok(views.html.disposal_of_vehicle.vehicle_lookup_failure(
+      data = vehicleLookUpFormModelDetails,
+      responseCodeVehicleLookupMSErrorMessage = responseCodeErrorMessage,
+      attempts = bruteForcePreventionResponse.attempts,
+      maxAttempts = bruteForcePreventionResponse.maxAttempts)
+    ).
+    discardingCookies(DiscardingCookie(name = VehicleLookupResponseCodeCacheKey))
   }
 
   private def encodeResponseCodeErrorMessage(implicit request: Request[AnyContent]): String =
