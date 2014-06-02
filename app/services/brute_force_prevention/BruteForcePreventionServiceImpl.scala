45f11f74e12905a9b4f5bc966ef0e03be18bb784
Scott Walker <scott.walker@valtech.co.uk>
2014-06-02 09:47:39 +0100

    US270 refactor into some encapsulated defs
    Former-commit-id: a8f5323b368f550a10311db7d6eac14b561a5825

--- app/services/brute_force_prevention/BruteForcePreventionServiceImpl.scala
+++ app/services/brute_force_prevention/BruteForcePreventionServiceImpl.scala
@@ -8,31 +8,37 @@ import utils.helpers.Config
 import models.domain.common.BruteForcePreventionResponse
 import models.domain.common.BruteForcePreventionResponse.JsonFormat
 import play.api.libs.json.Json
+import play.api.libs.ws.Response
 
 final class BruteForcePreventionServiceImpl @Inject()(ws: BruteForcePreventionWebService) extends BruteForcePreventionService {
   override def vrmLookupPermitted(vrm: String): Future[Option[(Boolean, BruteForcePreventionResponse)]] =
+  // TODO US270 this if-statement is a temporary feature toggle until all developers have Redis setup locally.
     if (Config.bruteForcePreventionEnabled) {
-      // TODO US270 this is temporary until we all developers have Redis setup locally.
       ws.callBruteForce(vrm).map {
         resp =>
+          def permitted(resp: Response) = {
+            val bruteForcePreventionResponse: Option[BruteForcePreventionResponse] = Json.fromJson[BruteForcePreventionResponse](resp.json).asOpt
+            bruteForcePreventionResponse match {
+              case Some(model) => Some((true, model))
+              case _ =>
+                Logger.error(s"Brute force prevention service returned invalid Json: ${resp.json}")
+                None
+            }
+          }
+          def notPermitted = Some((false, BruteForcePreventionResponse(attempts = 0, maxAttempts = 0)))
+          def unknownPermission(resp: Response) = {
+            Logger.error(s"Brute force prevention service returned status: ${resp.status}")
+            None
+          }
+
           resp.status match {
-            case play.api.http.Status.OK =>
-              val bruteForcePreventionResponse: Option[BruteForcePreventionResponse] = Json.fromJson[BruteForcePreventionResponse](resp.json).asOpt
-              bruteForcePreventionResponse match {
-                case Some(model) =>
-                  Some((true, model))
-                case _ =>
-                  Logger.error(s"Brute force prevention service returned invalid Json: ${resp.json}")
-                  None
-              }
-            case play.api.http.Status.FORBIDDEN => Some((false, BruteForcePreventionResponse(attempts = 0, maxAttempts = 0)))
-            case _ =>
-              Logger.error(s"Brute force prevention service returned status: ${resp.status}")
-              None
+            case play.api.http.Status.OK => permitted(resp)
+            case play.api.http.Status.FORBIDDEN => notPermitted
+            case _ => unknownPermission(resp)
           }
       }.recover {
         case e: Throwable =>
-          Logger.error(s"Brute force prevention service error: $e")
+          Logger.error(s"Brute force prevention service error: ${e.getStackTraceString}")
           None
       }
     }
