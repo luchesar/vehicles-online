c16c92001c23d60067a78b6e7bbffadc19b0be29
Geraint Jones <geraint@Geraints-MacBook-Air.local>
2014-06-03 10:25:07 +0100

    create a custom csrf token provider and store the toekn in a cookie as opposed to the default ie in the session
    Former-commit-id: fd79308bb5d2fbf91dcc19e8cf1a64efa6563d48

--- app/composition/Composition.scala
+++ app/composition/Composition.scala
@@ -1,9 +1,30 @@
 package composition
 
 import com.google.inject.Guice
-import play.api.mvc.EssentialFilter
-//import play.filters.gzip.GzipFilter
+import play.api.mvc.{Session, RequestHeader, EssentialFilter}
+import play.filters.csrf.CSRF._
 import play.filters.csrf.CSRFFilter
+import java.security.SecureRandom
+import org.apache.commons.codec.binary.Hex
+import play.api.libs.Crypto
+
+
+object CustomSignedTokenProvider extends TokenProvider {
+
+  def generateCryptoToken = {
+    val bytes = new Array[Byte](12)
+    new SecureRandom().nextBytes(bytes)
+    new String(Hex.encodeHex(bytes))
+  }
+
+  def generateSignedToken = Crypto.signToken(generateCryptoToken)
+  def generateToken = generateSignedToken
+  def compareTokens(tokenA: String, tokenB: String) = {
+    Crypto.compareSignedTokens(tokenA, tokenB)
+  }
+
+}
+
 
 object Composition {
   /**
@@ -22,7 +43,6 @@ object Composition {
    */
   lazy val devInjector = Guice.createInjector(DevModule)
 
-  lazy val filters: EssentialFilter = new CSRFFilter()
-//  lazy val filters: EssentialFilter = new GzipFilter()
+  lazy val filters: EssentialFilter = new CSRFFilter(tokenProvider = CustomSignedTokenProvider)
 
 }
