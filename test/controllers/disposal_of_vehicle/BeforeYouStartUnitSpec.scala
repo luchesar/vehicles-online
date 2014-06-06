cad1ca9eab5fbc5937a3a4b6262cde5cdca81d74
Scott Walker <scott.walker@valtech.co.uk>
2014-06-06 11:00:38 +0100

    US339: unit spec for withEn
    Former-commit-id: 37f7d1c0b5a791334cf674b9e3d99dccd270db0b

--- test/controllers/disposal_of_vehicle/BeforeYouStartUnitSpec.scala
+++ test/controllers/disposal_of_vehicle/BeforeYouStartUnitSpec.scala
@@ -6,6 +6,13 @@ import helpers.WithApplication
 import pages.disposal_of_vehicle._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import common.CookieHelper._
+import scala.Some
+import mappings.disposal_of_vehicle.BusinessChooseYourAddress._
+import scala.Some
+import mappings.disposal_of_vehicle.TraderDetails._
+import scala.Some
+import play.api.Play
 
 final class BeforeYouStartUnitSpec extends UnitSpec {
   "present" should {
@@ -20,6 +27,30 @@ final class BeforeYouStartUnitSpec extends UnitSpec {
       val result = beforeYouStart.submit(newFakeRequest)
       whenReady(result) {
         r => r.header.headers.get(LOCATION) should equal(Some(SetupTradeDetailsPage.address))
+      }
+    }
+  }
+
+  "withLanguageEn" should {
+    "redirect back to the same page" in new WithApplication {
+      val result = beforeYouStart.withLanguageEn(newFakeRequest)
+      whenReady(result) {
+        r =>
+          r.header.status should equal(SEE_OTHER) // Redirect...
+          r.header.headers.get(LOCATION) should equal(Some(BeforeYouStartPage.address)) // ... back to the same page.
+      }
+    }
+
+    "writes language cookie" in new WithApplication {
+      val result = beforeYouStart.withLanguageEn(newFakeRequest)
+      status(result) should equal(SEE_OTHER)
+      whenReady(result) {
+        r =>
+          val cookies = fetchCookiesFromHeaders(r)
+          cookies.find(_.name == Play.langCookieName) match {
+            case Some(cookie) => cookie.value should equal("en")
+            case None => fail("langCookieName not found")
+          }
       }
     }
   }
