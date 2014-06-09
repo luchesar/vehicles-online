d8d28329c63b6e3a6f4adbafcd0536b67d55bc38
Scott Walker <scott.walker@valtech.co.uk>
2014-06-09 13:38:20 +0100

    US138: language toggle on DuplicateDisposalError Page
    Former-commit-id: 12cf8bb7dbb7ae035e85b36154dfce339a93efb4

--- test/controllers/disposal_of_vehicle/DuplicateDisposalErrorUnitSpec.scala
+++ test/controllers/disposal_of_vehicle/DuplicateDisposalErrorUnitSpec.scala
@@ -4,14 +4,65 @@ import helpers.UnitSpec
 import play.api.test.Helpers._
 import play.api.test.FakeRequest
 import helpers.WithApplication
+import pages.disposal_of_vehicle.{DuplicateDisposalErrorPage, BeforeYouStartPage}
+import common.CookieHelper._
+import scala.Some
+import play.api.Play
 
 final class DuplicateDisposalErrorUnitSpec extends UnitSpec {
   "present" should {
     "display the page" in new WithApplication {
-      val duplicateDisposalError = new DuplicateDisposalError()
       val newFakeRequest = FakeRequest()
       val result = duplicateDisposalError.present(newFakeRequest)
       status(result) should equal(OK)
     }
   }
+
+  "withLanguageCy" should {
+    "redirect back to the same page" in new WithApplication {
+      val result = duplicateDisposalError.withLanguageCy(FakeRequest())
+      whenReady(result) {
+        r =>
+          r.header.status should equal(SEE_OTHER) // Redirect...
+          r.header.headers.get(LOCATION) should equal(Some(DuplicateDisposalErrorPage.address)) // ... back to the same page.
+      }
+    }
+
+    "writes language cookie set to 'cy'" in new WithApplication {
+      val result = duplicateDisposalError.withLanguageCy(FakeRequest())
+      whenReady(result) {
+        r =>
+          val cookies = fetchCookiesFromHeaders(r)
+          cookies.find(_.name == Play.langCookieName) match {
+            case Some(cookie) => cookie.value should equal("cy")
+            case None => fail("langCookieName not found")
+          }
+      }
+    }
+  }
+
+  "withLanguageEn" should {
+    "redirect back to the same page" in new WithApplication {
+      val result = duplicateDisposalError.withLanguageEn(FakeRequest())
+      whenReady(result) {
+        r =>
+          r.header.status should equal(SEE_OTHER) // Redirect...
+          r.header.headers.get(LOCATION) should equal(Some(DuplicateDisposalErrorPage.address)) // ... back to the same page.
+      }
+    }
+
+    "writes language cookie set to 'en'" in new WithApplication {
+      val result = duplicateDisposalError .withLanguageEn(FakeRequest())
+      whenReady(result) {
+        r =>
+          val cookies = fetchCookiesFromHeaders(r)
+          cookies.find(_.name == Play.langCookieName) match {
+            case Some(cookie) => cookie.value should equal("en")
+            case None => fail("langCookieName not found")
+          }
+      }
+    }
+  }
+
+  val duplicateDisposalError = new DuplicateDisposalError()
 }
