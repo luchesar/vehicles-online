89c8287023c5d2606e7fd45d17c5f8d62b10341f
Scott Walker <scott.walker@valtech.co.uk>
2014-06-09 13:19:04 +0100

    Refactor: add model name to failure message
    Former-commit-id: 3e87b987fb7129873e9ad5e77e0d504d659bde43

--- test/helpers/JsonUtils.scala
+++ test/helpers/JsonUtils.scala
@@ -1,16 +1,17 @@
 package helpers
 
-import play.api.libs.json.{Json, Reads}
+import models.domain.common.CacheKey
 import org.scalatest.Assertions.fail
+import play.api.libs.json.{Json, Reads}
 
 object JsonUtils {
 
-  def deserializeJsonToModel[A](json: String)(implicit fjs: Reads[A]): A = {
+  def deserializeJsonToModel[A](json: String)(implicit fjs: Reads[A], cacheKey: CacheKey[A]): A = {
     val parsedJsValue = Json.parse(json)
     val fromJson = Json.fromJson[A](parsedJsValue)
     fromJson.asEither match {
       case Left(errors) =>
-        fail(s"Failed to deserialize this json: $json into a model")
+        fail(s"Failed to deserialize this json: $json into a model of type ${cacheKey.value}")
       case Right(model) =>
         model
     }
