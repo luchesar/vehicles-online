16346278b181ae3adfafbf8dcf8124cbd0a06dc0
Ian Stainer <ian.stainer@valtech.co.uk>
2014-06-12 15:12:50 +0100

    Code tidy up
    Former-commit-id: 3f69b39f7056f1d3cf7a0a4097fd2f238cf50a31

--- app/services/brute_force_prevention/WebServiceImpl.scala
+++ app/services/brute_force_prevention/WebServiceImpl.scala
@@ -9,6 +9,7 @@ import com.google.inject.Inject
 final class WebServiceImpl @Inject()(config: Config) extends BruteForcePreventionWebService {
   private val baseUrl: String = config.bruteForcePreventionMicroServiceBaseUrl
   private val requestTimeout: Int = config.bruteForcePreventionTimeout
+  private val serviceName: String = config.bruteForcePreventionServiceNameHeader
   private val maxRetries: String = config.bruteForcePreventionMaxAttemptsHeader.toString
   private val keyExpire: String = config.bruteForcePreventionExpiryHeader
 
@@ -16,7 +17,7 @@ final class WebServiceImpl @Inject()(config: Config) extends BruteForcePreventio
     val endPoint = s"$baseUrl/security"
     Logger.debug(s"Calling brute force prevention on $endPoint with vrm: $vrm")
     WS.url(endPoint).
-      withHeaders("serviceName" -> config.bruteForcePreventionServiceNameHeader).
+      withHeaders("serviceName" -> serviceName).
       withHeaders("maxRetries" -> maxRetries).
       withHeaders("keyExpire" -> keyExpire).
       withRequestTimeout(requestTimeout). // Timeout is in milliseconds
