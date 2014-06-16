1244ed63ff62d62c8716893f77339409618095e9
Scott Walker <scott.walker@valtech.co.uk>
2014-06-16 14:25:47 +0100

    Upgraded guice version
    Former-commit-id: 9ad10eccf1e9015657dd604b30466083a3a8a2a0

--- project/Build.scala
+++ project/Build.scala
@@ -23,7 +23,7 @@ object ApplicationBuild extends Build {
     "org.mockito" % "mockito-all" % "1.9.5" % "test" withSources() withJavadoc(),
     "com.github.tomakehurst" % "wiremock" % "1.46" % "test" withSources() withJavadoc(),
     "org.scalatest" % "scalatest_2.10" % "2.2.0" % "test" withSources() withJavadoc(),
-    "com.google.inject" % "guice" % "4.0-beta" withSources() withJavadoc(),
+    "com.google.inject" % "guice" % "4.0-beta4" withSources() withJavadoc(),
     "com.tzavellas" % "sse-guice" % "0.7.1" withSources() withJavadoc(), // Scala DSL for Guice
     "commons-codec" % "commons-codec" % "1.9" withSources() withJavadoc(),
     "org.apache.httpcomponents" % "httpclient" % "4.3.1" withSources() withJavadoc()
