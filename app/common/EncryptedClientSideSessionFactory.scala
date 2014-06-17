2cfe859da0e097d1624040e885822bfbe192462f
Lawrence Wagerfield <lawrence@dmz.wagerfield.com>
2014-06-17 09:11:20 +0100

    Improved code readability (some more)
    Former-commit-id: ca13345f90ef6b7d401848e7fd49094dd6045062

--- app/common/EncryptedClientSideSessionFactory.scala
+++ app/common/EncryptedClientSideSessionFactory.scala
@@ -10,27 +10,23 @@ import play.api.mvc.Cookie
 class EncryptedClientSideSessionFactory @Inject()()(implicit cookieFlags: CookieFlags,
                                                     encryption: CookieEncryption,
                                                     cookieNameHashing: CookieNameHashing) extends ClientSideSessionFactory {
-
   /**
    * Session secret key must not expire before any other cookie that relies on it.
    */
   private final val SessionSecretKeyLifetime = None
+  private val secureCookies: Boolean = getProperty("secureCookies", default = true)
+  private val sessionSecretKeySuffixKey: String = getProperty("sessionSecretKeySuffixKey", "FE291934-66BD-4500-B27F-517C7D77F26B")
 
-  val secureCookies: Boolean = getProperty("secureCookies", default = true)
-  val sessionSecretKeySuffixKey: String = getProperty("sessionSecretKeySuffixKey", "FE291934-66BD-4500-B27F-517C7D77F26B")
-
-  override def newSessionCookiesIfNeeded(request: Traversable[Cookie]): Option[Seq[Cookie]] = {
+  override def newSessionCookiesIfNeeded(request: Traversable[Cookie]): Option[Seq[Cookie]] =
     validateSessionCookies(request) match {
       case Some((trackingId, sessionSecretKey)) => None
       case _ =>
         val sessionSecretKey = newSessionSecretKey
-
-        val trackingIdCookieName = ClientSideSessionFactory.TrackingIdCookieName
         val sessionSecretKeyCipherText = encryption.encrypt(createSessionSecretKeySuffixCookieName + sessionSecretKey)
         val (prefixValue, suffixValue) = sessionSecretKeyCipherText.splitAt(20)
 
         val trackingIdCookie = Cookie(
-          name = trackingIdCookieName,
+          name = ClientSideSessionFactory.TrackingIdCookieName,
           value = prefixValue,
           secure = secureCookies,
           maxAge = SessionSecretKeyLifetime)
@@ -43,20 +39,18 @@ class EncryptedClientSideSessionFactory @Inject()()(implicit cookieFlags: Cookie
 
         Some(Seq(trackingIdCookie, sessionSecretKeySuffixCookie))
     }
-  }
 
-  override def getSession(request: Traversable[Cookie]): ClientSideSession = {
+  override def getSession(request: Traversable[Cookie]): ClientSideSession =
     validateSessionCookies(request) match {
       case Some((trackingId, sessionSecretKey)) =>
         new EncryptedClientSideSession(trackingId, sessionSecretKey)
-      case _ => throw new InvalidSessionException("No session present in the request")
+      case _ =>
+        throw new InvalidSessionException("No session present in the request")
     }
-  }
 
   private def validateSessionCookies(requestCookies: Traversable[Cookie]): Option[(String, String)] = {
-    val sessionSecretKeySuffixCookieName = createSessionSecretKeySuffixCookieName
-
     val trackingIdCookie = requestCookies.find(_.name == ClientSideSessionFactory.TrackingIdCookieName)
+    val sessionSecretKeySuffixCookieName = createSessionSecretKeySuffixCookieName
     val sessionSecretKeySuffixCookie = requestCookies.find(_.name == sessionSecretKeySuffixCookieName)
 
     (trackingIdCookie, sessionSecretKeySuffixCookie) match {
@@ -75,10 +69,11 @@ class EncryptedClientSideSessionFactory @Inject()()(implicit cookieFlags: Cookie
     }
   }
 
-  private def createSessionSecretKeySuffixCookieName(implicit cookieNameHashing: CookieNameHashing): String =
+  private def createSessionSecretKeySuffixCookieName: String =
     cookieNameHashing.hash(sessionSecretKeySuffixKey)
 
-  private def newSessionSecretKey = Hex.encodeHexString(getSecureRandomBytes(16))
+  private def newSessionSecretKey: String =
+    Hex.encodeHexString(getSecureRandomBytes(16))
 
   private def getSecureRandomBytes(numberOfBytes: Int): Array[Byte] = {
     val random = new SecureRandom()
