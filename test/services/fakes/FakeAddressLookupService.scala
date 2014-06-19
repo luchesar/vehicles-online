8696b521132f254dd573ecbe627c23a5250d926c
Scott Walker <scott.walker@valtech.co.uk>
2014-06-19 12:36:51 +0100

    Refactor: fix warning about forward ref
    Former-commit-id: 9fa00d7f145eb99aa7254685c8a7188331f96db3

--- test/services/fakes/FakeAddressLookupService.scala
+++ test/services/fakes/FakeAddressLookupService.scala
@@ -6,13 +6,13 @@ import models.domain.disposal_of_vehicle.AddressViewModel
 object FakeAddressLookupService {
   final val   TraderBusinessNameValid = "example trader name"
   final val PostcodeInvalid = "xx99xx"
+  final val PostcodeValid = "CM81QJ"
   val addressWithoutUprn = AddressViewModel(address = Seq("44 Hythe Road", "White City", "London", PostcodeValid))
   val addressWithUprn = AddressViewModel(uprn = Some(traderUprnValid), address = Seq("44 Hythe Road", "White City", "London", PostcodeValid))
   final val BuildingNameOrNumberValid = "1234"
   final val Line2Valid = "line2 stub"
   final val Line3Valid = "line3 stub"
   final val PostTownValid = "postTown stub"
-  final val PostcodeValid = "CM81QJ"
 
   final val PostcodeValidWithSpace = "CM8 1QJ"
   final val PostcodeNoResults = "SA99 1DD"
