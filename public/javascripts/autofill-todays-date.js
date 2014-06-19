8861ea907ec01f54183a713c44543dfd6bb397fe
Scott Walker <scott.walker@valtech.co.uk>
2014-06-19 09:15:19 +0100

    US432: pass ids into javaScript, correct message file
    Former-commit-id: d96bb01f3859caa48345073b0844f43e0c9c8396

--- public/javascripts/autofill-todays-date.js
+++ public/javascripts/autofill-todays-date.js
@@ -1,17 +1,16 @@
-function AutoFillTodaysDate(day, month, year) {
+function AutoFillTodaysDate(day, month, year, checkboxId, dayId, monthId, yearId) {
+    var dod_elm = document.getElementById(checkboxId);
+    var dod_day = document.getElementById(dayId);
+    var dod_month = document.getElementById(monthId);
+    var dod_year = document.getElementById(yearId);
 
-	var dod_elm = document.getElementById("todaysDateOfDisposal");
-	var dod_day = document.getElementById("dateOfDisposal_day");
-	var dod_month = document.getElementById("dateOfDisposal_month");
-	var dod_year = document.getElementById("dateOfDisposal_year");
-
-  if (dod_elm.checked === false) {
-    dod_day.value = '';
-    dod_month.value = '';
-    dod_year.value = '';
-  } else {
-    dod_day.value = day;
-    dod_month.value = month;
-    dod_year.value = year;
-  }
+    if (dod_elm.checked === false) {
+        dod_day.value = '';
+        dod_month.value = '';
+        dod_year.value = '';
+    } else {
+        dod_day.value = day;
+        dod_month.value = month;
+        dod_year.value = year;
+    }
 }
