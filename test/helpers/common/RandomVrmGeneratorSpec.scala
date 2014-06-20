1f8164ec58729fd8d8018268f94f42ee8b8eadc6
Ian Stainer <ian.stainer@valtech.co.uk>
2014-06-20 20:28:39 +0100

    Changes and more tests for the random vrm generator
    Former-commit-id: 3eb922234beff56b90ce45a5838f23ed632a2dc0

---
+++ test/helpers/common/RandomVrmGeneratorSpec.scala
@@ -0,0 +1,24 @@
+package helpers.common
+
+import helpers.UnitSpec
+
+class RandomVrmGeneratorSpec extends UnitSpec {
+
+  "Random vrm generator" should {
+    "not generate the same vrm twice" in {
+      val first = RandomVrmGenerator.vrm
+      val second = RandomVrmGenerator.vrm
+      first should not equal(second)
+    }
+
+    "not use the same character in each section of the vrm eg AA11BBB" in {
+      val vrm = RandomVrmGenerator.vrm
+
+      // If the number of distinct characters is not > 3 this means each of the 3 sections
+      // has the same character used multiple times and so the randomization is not enough
+      vrm.distinct.size > 3 should equal(true)
+    }
+
+  }
+
+}
