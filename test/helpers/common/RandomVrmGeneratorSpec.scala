b765b03aa1654c4c4e2c9bf143564bb60c7f9a84
Ian Stainer <ian.stainer@valtech.co.uk>
2014-06-23 08:52:28 +0100

    Added test
    Former-commit-id: 6d5f645da0f5c0a5b6bbbf8debb8baacfb42d5b9

--- test/helpers/common/RandomVrmGeneratorSpec.scala
+++ test/helpers/common/RandomVrmGeneratorSpec.scala
@@ -11,5 +11,13 @@ final class RandomVrmGeneratorSpec extends UnitSpec {
       first should not equal second
     }
 
+    "not use the same character in each section of the vrm eg AA11BBB" in {
+      val vrm = RandomVrmGenerator.vrm
+
+      // If the number of distinct characters is not > 3 this means each of the 3 sections
+      // has the same character used multiple times and so the randomization is not enough
+      vrm.distinct.size > 3 should equal(true)
+    }
+
   }
 }
