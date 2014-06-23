b72d5a9b3cd03279bf5f7b8aa60e94286ba0e0b8
Ian Stainer <ian.stainer@valtech.co.uk>
2014-06-23 10:46:18 +0100

    Replaced implicit session that contains the tracking id with explicit tracking id
    Former-commit-id: dce99fc6e4d02a70f18feabd31b18b5913382e10

--- app/services/address_lookup/AddressLookupWebService.scala
+++ app/services/address_lookup/AddressLookupWebService.scala
@@ -2,15 +2,14 @@ package services.address_lookup
 
 import scala.concurrent.Future
 import play.api.libs.ws.Response
-import common.ClientSideSession
 import play.api.i18n.Lang
 
 // Wrapper around our webservice call so that we can IoC fake versions for testing or use the real version.
 trait AddressLookupWebService {
 
-  def callPostcodeWebService(postcode: String)
-                            (implicit session: ClientSideSession, lang: Lang): Future[Response]
+  def callPostcodeWebService(postcode: String, trackingId: String)
+                            (implicit lang: Lang): Future[Response]
 
-  def callUprnWebService(uprn: String)
-                        (implicit session: ClientSideSession, lang: Lang): Future[Response]
+  def callUprnWebService(uprn: String, trackingId: String)
+                        (implicit lang: Lang): Future[Response]
 }
