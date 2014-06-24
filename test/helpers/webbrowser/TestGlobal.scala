5e8edd9119f1870e953fd5186fd44a0adc9ed758
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-06-24 08:47:55 +0100

    Testing exception cases with access logging
    Former-commit-id: 638eb63559520ed7f37ed3b1de7a1f1e987ab936

--- test/helpers/webbrowser/TestGlobal.scala
+++ test/helpers/webbrowser/TestGlobal.scala
@@ -5,14 +5,16 @@ import java.util.UUID
 
 import com.google.inject.Injector
 import com.typesafe.config.ConfigFactory
-import composition.TestComposition._
+import composition.TestComposition.{filters => appFilters, _}
 import play.api._
 import play.api.mvc.Results._
 import play.api.mvc._
+import services.csrf_prevention.CSRFPreventionFilter
 import utils.helpers.ErrorStrategy
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import filters._
 
 /**
  * Application configuration is in a hierarchy of files:
@@ -29,7 +31,7 @@ import scala.concurrent.Future
  * play -Dconfig.file=conf/application.test.conf run
  */
 
-object TestGlobal extends WithFilters(filters: _*) with GlobalSettings {
+object TestGlobal extends GlobalSettings {
   private lazy val injector: Injector = testInjector
 
   /**
@@ -52,5 +54,18 @@ object TestGlobal extends WithFilters(filters: _*) with GlobalSettings {
 
   override def onError(request: RequestHeader, ex: Throwable): Future[SimpleResult] =
     ErrorStrategy(request, ex)
+
+  override def doFilter(a: EssentialAction): EssentialAction = {
+    // TODO can we work out how to dependency inject into WithFilters?
+    // EnsureSessionCreatedFilter relies on IoC which in turn requires the configs to be read, but if we try to load it in the
+    // Global WithFilters the config is not yet loaded so will fail. One solution is to use this doFilter to append to the
+    // Global filters.
+    Filters(
+      super.doFilter(a),
+      testInjector.getInstance(classOf[EnsureSessionCreatedFilter]),
+      testInjector.getInstance(classOf[AccessLoggingFilter]),
+      new CSRFPreventionFilter()
+    )
+  }
 }
 
