4cd1ddd7a0a8b0a85dfb96a2cde1d7dfd71f10a3
Scott Walker <scott.walker@valtech.co.uk>
2014-06-27 10:49:22 +0100

    Test for display prototype and progress bar
    Former-commit-id: 8311402bff55d52ff56c9accd8c696cec3ef1f49

--- test/controllers/disposal_of_vehicle/DuplicateDisposalErrorUnitSpec.scala
+++ test/controllers/disposal_of_vehicle/DuplicateDisposalErrorUnitSpec.scala
@@ -1,8 +1,11 @@
 package controllers.disposal_of_vehicle
 
+import common.ClientSideSessionFactory
 import helpers.{UnitSpec, WithApplication}
+import org.mockito.Mockito._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import utils.helpers.Config
 
 final class DuplicateDisposalErrorUnitSpec extends UnitSpec {
   "present" should {
@@ -16,6 +19,17 @@ final class DuplicateDisposalErrorUnitSpec extends UnitSpec {
 
     "display prototype message when config set to true" in new WithApplication {
       contentAsString(present) should include("""<div class="prototype">""")
+    }
+
+    "not display prototype message when config set to false" in new WithApplication {
+      val request = FakeRequest()
+      implicit val clientSideSessionFactory = injector.getInstance(classOf[ClientSideSessionFactory])
+      implicit val config: Config = mock[Config]
+      when(config.isPrototypeBannerVisible).thenReturn(false)
+      val duplicateDisposalErrorPrototypeNotVisible = new DuplicateDisposalError()
+
+      val result = duplicateDisposalErrorPrototypeNotVisible.present(request)
+      contentAsString(result) should not include """<div class="prototype">"""
     }
   }
 
