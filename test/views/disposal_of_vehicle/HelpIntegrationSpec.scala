f00704a3791e2d96a26fd31bcd4d076297a8f7cf
Matthew Snaith <fault_line@hotmail.co.uk>
2014-06-27 11:56:37 +0100

    further tests added for back button in HelpIntegrationSpec
    Former-commit-id: 41e6193a902c2a9c431327a15557a05f5e28ff09

--- test/views/disposal_of_vehicle/HelpIntegrationSpec.scala
+++ test/views/disposal_of_vehicle/HelpIntegrationSpec.scala
@@ -2,9 +2,12 @@ package views.disposal_of_vehicle
 
 import helpers.UiSpec
 import helpers.tags.UiTag
-import helpers.webbrowser.TestHarness
+import helpers.webbrowser.{WebDriverFactory, TestHarness}
 import pages.disposal_of_vehicle._
-import pages.disposal_of_vehicle.HelpPage.exit
+import pages.disposal_of_vehicle.HelpPage.{exit, back}
+import org.openqa.selenium.WebDriver
+import helpers.disposal_of_vehicle.CookieFactoryForUISpecs
+
 
 final class HelpIntegrationSpec extends UiSpec with TestHarness {
    "go to page" should {
@@ -15,6 +18,35 @@ final class HelpIntegrationSpec extends UiSpec with TestHarness {
      }
    }
 
+  "back button" should {
+    "redirect to the users previous page with javascript enabled " taggedAs UiTag in new WebBrowser(
+        webDriver = WebDriverFactory.webDriver(targetBrowser = "htmlUnit", javascriptEnabled = true)) {
+
+      go to BeforeYouStartPage
+      cacheSetup()
+
+      go to VehicleLookupPage
+
+      go to HelpPage
+
+      click on back
+
+      page.title should equal(VehicleLookupPage.title)
+    }
+
+    "have no functionality with javascript disabled" taggedAs UiTag in new WebBrowser(
+      webDriver = WebDriverFactory.webDriver(targetBrowser = "htmlUnit", javascriptEnabled = false)) {
+
+      go to BeforeYouStartPage
+
+      go to HelpPage
+
+      click on back
+
+      page.title should equal(HelpPage.title)
+    }
+  }
+
   "exit" should {
     "leave the help page and display the start page" taggedAs UiTag in new WebBrowser {
       go to HelpPage
@@ -24,4 +56,9 @@ final class HelpIntegrationSpec extends UiSpec with TestHarness {
       page.title should equal(BeforeYouStartPage.title)
     }
   }
+
+  private def cacheSetup()(implicit webDriver: WebDriver) =
+    CookieFactoryForUISpecs.
+      setupTradeDetails().
+      dealerDetails()
 }
