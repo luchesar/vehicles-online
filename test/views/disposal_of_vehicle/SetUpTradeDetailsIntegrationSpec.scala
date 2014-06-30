9cced94eb4bea31f4c3aafe1aace194c3130e34b
Scott Walker <scott.walker@valtech.co.uk>
2014-06-30 10:45:42 +0100

    Refactor: test redundant as they are now done once in MainUiSpec
    Former-commit-id: e60a61419e026cad59ab90832baeb7ba0a38a3fe

--- test/views/disposal_of_vehicle/SetUpTradeDetailsIntegrationSpec.scala
+++ test/views/disposal_of_vehicle/SetUpTradeDetailsIntegrationSpec.scala
@@ -32,24 +32,6 @@ final class SetUpTradeDetailsIntegrationSpec extends UiSpec with TestHarness {
       csrf.getAttribute("name") should equal(services.csrf_prevention.CSRFPreventionAction.csrfPreventionTokenName)
       csrf.getAttribute("value").size > 0 should equal(true)
     }
-
-    "display the 'Cymraeg' language button and not the 'English' language button when the language cookie is set to 'en'" taggedAs UiTag ignore new WebBrowser {
-      go to BeforeYouStartPage // By default will load in English.
-      CookieFactoryForUISpecs.withLanguageEn()
-      go to SetupTradeDetailsPage
-
-      hasCymraeg should equal(true)
-      hasEnglish should equal(false)
-    }
-
-    "display the 'English' language button and not the 'Cymraeg' language button when the language cookie is set to 'cy'" taggedAs UiTag ignore new WebBrowser {
-      go to BeforeYouStartPage // By default will load in English.
-      CookieFactoryForUISpecs.withLanguageCy()
-      go to SetupTradeDetailsPage
-
-      hasCymraeg should equal(false)
-      hasEnglish should equal(true)
-    }
   }
 
   "lookup button" should {
