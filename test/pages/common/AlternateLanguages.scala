054b8f8ef051c7319d9e266468001c1670add245
Scott Walker <scott.walker@valtech.co.uk>
2014-06-30 09:16:17 +0100

    Refactor: Moved tests for mailto, languages and prototype message to an MainUiSpec as they are not specific to one page. Moved def for finding Element mailto to a Feedback class
    Former-commit-id: bb9880bc90769fa37ed074ed064e4e50a04840f1

--- test/pages/common/AlternateLanguages.scala
+++ test/pages/common/AlternateLanguages.scala
@@ -1,15 +1,15 @@
 package pages.common
 
 import helpers.webbrowser.{Element, WebBrowserDSL}
-import org.openqa.selenium.WebDriver
 import mappings.common.AlternateLanguages._
-import mappings.common.Feedback.FeedbackId
+import org.openqa.selenium.WebDriver
 
-object AlternateLanguages extends WebBrowserDSL{
+object AlternateLanguages extends WebBrowserDSL {
   def cymraeg(implicit driver: WebDriver): Element = find(id(CyId)).get
+
   def english(implicit driver: WebDriver): Element = find(id(EnId)).get
 
   def hasCymraeg(implicit driver: WebDriver): Boolean = find(id(CyId)).isDefined
+
   def hasEnglish(implicit driver: WebDriver): Boolean = find(id(EnId)).isDefined
-  def mailto(implicit driver: WebDriver): Element = find(id(FeedbackId)).get
 }
