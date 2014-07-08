218d82987bc73396318274eff708267362612352
Ian Stainer <ian.stainer@valtech.co.uk>
2014-07-08 11:03:36 +0100

    Code tidy up removed underscore from imports

--- app/filters/csrf_prevention/CsrfPreventionFilter.scala
+++ app/filters/csrf_prevention/CsrfPreventionFilter.scala
@@ -2,7 +2,7 @@ package filters.csrf_prevention
 
 import com.google.inject.Inject
 import common.ClientSideSessionFactory
-import play.api.mvc._
+import play.api.mvc.{EssentialAction, EssentialFilter}
 
 class CsrfPreventionFilter @Inject()
                            (implicit clientSideSessionFactory: ClientSideSessionFactory) extends EssentialFilter {
